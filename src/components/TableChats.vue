a8161cf15e97427a789307521d90c63d8d958ddf
Mariana <moraismariana83@gmail.com>
2024-01-25 08:49:15 -0300

    Add the component Table Chats

---
+++ src/components/TableChats.vue
@@ -0,0 +1,137 @@
+<template>
+  <section class="table-chats">
+    <header class="table-chats__tab-buttons">
+      <button
+        v-for="(tab, index) in tabs"
+        :key="index"
+        @click="changeTab(index)"
+        :class="{ active: activeTab === index }"
+        class="table-chats__tab-button table-chats__tab-button-active"
+      >
+        {{ tab }}
+      </button>
+    </header>
+    <table class="table-chats__table">
+      <thead class="table-chats__table-head">
+        <tr>
+          <th v-for="header in headers" :key="header">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody class="table-chats__table-body">
+        <tr
+          v-for="(row, rowIndex) in data[activeTab]"
+          :key="rowIndex"
+          class="table-chats__table-row"
+        >
+          <td v-for="(value, colIndex) in row" :key="colIndex">{{ value }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <hr class="table-chats__separator" />
+    <footer class="table-chats__bottom">
+      <p>1 - 10 de 125</p>
+      <unnnic-pagination v-model="page" :max="10" :show="5" />
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TableChats',
+
+  data: () => ({
+    activeTab: 0,
+    page: 1,
+    tabs: ['Aguardando', 'Em andamento', 'Encerrados'],
+    headers: ['Contato', 'Agente', 'Horário de início', 'Tempo de interação'],
+    data: [
+      [
+        ['Fernanda da Silva Santos', 'Tamara', '12:59', '01:16'],
+        ['Fernanda da Silva Santos', 'Tamara', '12:59', '01:16'],
+        ['Fernanda da Silva Santos', 'Tamara', '12:59', '01:16'],
+        ['Fernanda da Silva Santos', 'Tamara', '12:59', '01:16'],
+        ['Fernanda da Silva Santos', 'Tamara', '12:59', '01:16'],
+      ],
+      [
+        ['Monica Cristina', 'Vitória', '12:59', '01:16'],
+        ['Monica Cristina', 'Vitória', '12:59', '01:16'],
+        ['Monica Cristina', 'Vitória', '12:59', '01:16'],
+        ['Monica Cristina', 'Vitória', '12:59', '01:16'],
+        ['Monica Cristina', 'Vitória', '12:59', '01:16'],
+      ],
+      [
+        ['Claudio Leal', 'Larissa', '12:59', '01:16'],
+        ['Claudio Leal', 'Larissa', '12:59', '01:16'],
+        ['Claudio Leal', 'Larissa', '12:59', '01:16'],
+        ['Claudio Leal', 'Larissa', '12:59', '01:16'],
+        ['Claudio Leal', 'Larissa', '12:59', '01:16'],
+      ],
+    ],
+  }),
+
+  methods: {
+    changeTab(index) {
+      this.activeTab = index;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table-chats {
+  background-color: $unnnic-color-neutral-white;
+  border: 1px solid $unnnic-color-neutral-cleanest;
+
+  &__tab-buttons {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+
+  &__tab-button {
+    width: 100%;
+    background-color: $unnnic-color-neutral-lightest;
+    border: 1px solid $unnnic-color-neutral-cleanest;
+    cursor: pointer;
+    text-align: left;
+    color: $unnnic-color-neutral-cloudy;
+    font-size: 16px;
+    font-weight: $unnnic-font-weight-bold;
+    padding: $unnnic-spacing-ant $unnnic-spacing-sm;
+  }
+
+  &__tab-button-active {
+    background-color: $unnnic-color-neutral-white;
+    color: $unnnic-color-neutral-darkest;
+  }
+  &__table {
+    width: -webkit-fill-available;
+    border-collapse: collapse;
+    color: $unnnic-color-neutral-black;
+    text-align: left;
+    margin: $unnnic-spacing-ant $unnnic-spacing-sm;
+    font-size: $unnnic-font-size-body-gt;
+  }
+  &__table-head {
+    margin-bottom: 1rem;
+  }
+  &__table-row {
+    height: $unnnic-spacing-lg;
+    margin: $unnnic-spacing-ant;
+    align-self: stretch;
+    color: $unnnic-color-neutral-dark;
+  }
+  &__separator {
+    border: 1px solid $unnnic-color-neutral-soft;
+    border-radius: $unnnic-spacing-nano;
+    height: 1px;
+    margin: $unnnic-spacing-ant $unnnic-spacing-sm;
+  }
+  &__bottom {
+    display: flex;
+    align-items: center;
+    margin: $unnnic-spacing-md $unnnic-spacing-sm;
+    place-content: space-between;
+  }
+}
+</style>

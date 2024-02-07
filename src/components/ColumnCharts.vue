c39839e0ae13da206c19f86604705855c8e1cf3f
Mariana <moraismariana83@gmail.com>
2024-02-07 17:07:53 -0300

    feat: Add ApexCharts library and the ColumnChart component

---
+++ src/components/ColumnCharts.vue
@@ -0,0 +1,71 @@
+<template>
+  <apexchart width="500" type="bar" :options="options" :series="series" />
+</template>
+
+<script>
+export default {
+  name: 'ColumnChart',
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    options: {
+      colors: ['#9CACCC'],
+      chart: {
+        id: 'column-chart',
+        toolbar: {
+          show: false,
+        },
+        fontFamily: 'Lato, sans-serif;',
+      },
+      grid: {
+        show: false,
+        xaxis: {
+          lines: {
+            show: false,
+          },
+        },
+        yaxis: {
+          lines: {
+            show: false,
+          },
+        },
+      },
+      xaxis: {
+        categories: [],
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      yaxis: {
+        show: false,
+      },
+      dataLabels: {
+        enabled: false,
+        enabledOnSeries: false,
+      },
+    },
+    series: [
+      {
+        name: 'Atendimentos',
+        data: [],
+      },
+    ],
+  }),
+  created() {
+    this.importData();
+  },
+  methods: {
+    importData() {
+      this.options.xaxis.categories = this.chartData.categories;
+      this.series[0].data = this.chartData.data;
+    },
+  },
+};
+</script>

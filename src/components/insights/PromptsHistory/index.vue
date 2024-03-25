6c9048bbed7f100ef67846ccccfadf15542d104d
aldemylla <aldemylla@gmail.com>
2024-03-25 14:53:30 -0300

    feat: Added gpt prompt history to ResizableBar

---
+++ src/components/insights/PromptsHistory/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="prompts-history">
+    <section
+      v-for="(insight, index) in insights"
+      class="prompts-history__conversation"
+      :key="index"
+    >
+      <PromptHistoryMessage
+        title="Você:"
+        :content="insight.request"
+      />
+      <PromptHistoryMessage
+        title="InsightsGPT:"
+        :content="insight.received"
+      />
+    </section>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import PromptHistoryMessage from './PromptHistoryMessage.vue';
+
+export default {
+  name: 'PromptsHistory',
+
+  components: {
+    PromptHistoryMessage,
+  },
+
+  computed: {
+    ...mapState({
+      insights: (state) => state.gpt.insights,
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.prompts-history {
+  display: grid;
+  gap: $unnnic-spacing-xl;
+
+  &__conversation {
+    display: grid;
+    gap: $unnnic-spacing-xl;
+  }
+}
+</style>

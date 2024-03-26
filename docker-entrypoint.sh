b25b1d4cf199d7cfa53a06cadc0df10548cbbdb7
Aldemylla <aldemylla@gmail.com>
2024-03-26 15:34:06 -0300

    fix: Config gpt env at docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-export JSON_STRING='window.configs = {}'
+export JSON_STRING='window.configs = { \
+  "VITE_GPT_URL":"'${VITE_GPT_URL}'", \
+  "VITE_GPT_AUTH":"'${VITE_GPT_AUTH}'", \
+}'
 sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html
 
 exec "$@"

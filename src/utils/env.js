522edd5f070375c9d201c5a689b8901d31cb9675
aldemylla <aldemylla@gmail.com>
2024-04-02 16:17:54 -0300

    fix: Getting VITE_GPT_URL directly

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,8 +2,6 @@ export default function env(name) {
   return (
     import.meta.env?.[name] ||
     window?.configs?.[name] ||
-    process.env[name] ||
-    window?.configs?.[`VITE_${name}`] ||
-    process.env[`VITE_${name}`]
+    window?.configs?.[`VITE_${name}`]
   );
 }

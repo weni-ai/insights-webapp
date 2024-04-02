4aae31a7fb12c5de782cd761db13b77ce209a96c
aldemylla <aldemylla@gmail.com>
2024-04-02 18:07:42 -0300

    feat: Added vite-plugin-env-compatible to use VUE_APP env prefix and process.env

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,25 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import env from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return defineConfig({
-    define: {
-      'process.env': env,
-    },
-    plugins: [vue(), vueJsx()],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `
+export default defineConfig({
+  plugins: [vue(), vueJsx(), env({ prefix: 'VUE_APP' })],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
           @import '@weni/unnnic-system/src/assets/scss/unnnic.scss';
         `,
-        },
       },
     },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  });
-};
+  },
+});

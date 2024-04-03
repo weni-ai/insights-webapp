de2aac6b51f12704c51b728599a1ea296bebb03f
aldemylla <aldemylla@gmail.com>
2024-04-02 21:05:27 -0300

    fix: Docker enviroment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export JSON_STRING='window.configs = { \
-  "VITE_GPT_URL":"'${VITE_GPT_URL}'", \
-  "VITE_GPT_AUTH":"'${VITE_GPT_AUTH}'", \
+  "VUE_APP_GPT_URL":"'${VUE_APP_GPT_URL}'", \
+  "VUE_APP_GPT_AUTH":"'${VUE_APP_GPT_AUTH}'", \
 }'
 sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html
 

e036925db2e7fdc815b48738d4d3c85cb4756de4
aldemylla <aldemylla@gmail.com>
2024-04-04 11:24:12 -0300

    feat: Adjusted docker env configs and removes vite-plugin-env-compatible lib

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
+
 #!/bin/sh
-export JSON_STRING='window.configs = { \
-  "VUE_APP_GPT_URL":"'${VUE_APP_GPT_URL}'", \
-  "VUE_APP_GPT_AUTH":"'${VUE_APP_GPT_AUTH}'", \
+JSON_STRING='window.configs = { \
+  "VITE_GPT_URL":"'${VITE_GPT_URL}'", \
+  "VITE_GPT_AUTH":"'${VITE_GPT_AUTH}'", \
 }'
-sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html
+sed "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html
 
 exec "$@"
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import env from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), env({ prefix: 'VUE_APP' })],
+  plugins: [vue(), vueJsx()],
   css: {
     preprocessorOptions: {
       scss: {

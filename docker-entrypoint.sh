b2d56d3e41f4de3f74f84c2186d76981a17c92f2
aldemylla <aldemylla@gmail.com>
2024-04-05 18:39:34 -0300

    fix: docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@ export JSON_STRING='window.configs = { \
   "VITE_GPT_URL":"'${VITE_GPT_URL}'", \
   "VITE_GPT_AUTH":"'${VITE_GPT_AUTH}'", \
 }'
-sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/smart-descriptions/index.html.tmpl > /tmp/index.html
+sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html
 
 exec "$@"

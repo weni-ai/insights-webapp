3e4500aa58feecae9b9150eb8053dcf3677bdd72
aldemylla <aldemylla@gmail.com>
2024-05-23 15:30:02 -0300

    feat: Start dashboard view and set its dynamic grid

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Dashboard from '@/views/insights/Home/index.vue';
+import Dashboard from '@/views/insights/Dashboard.vue';
 import routes from './routes';
 
 const router = createRouter({
---
+++ src/views/insights/Dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+  <section
+    class="dashboard"
+    ref="refDashboard"
+  >
+    oi
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'DashboardView',
+
+  mounted() {
+    this.setDashboardStyleGrid();
+  },
+
+  computed: {
+    ...mapState({
+      currentDashboard: (state) => state.dashboards.currentDashboard,
+    }),
+  },
+
+  methods: {
+    setDashboardStyleGrid() {
+      const { grid } = this.currentDashboard || {};
+      const { refDashboard } = this.$refs;
+
+      if (grid) {
+        refDashboard.style.grid = `${grid.rows}fr / ${grid.columns}fr`;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  overflow: hidden;
+
+  height: 100%;
+
+  display: grid;
+  gap: $unnnic-spacing-sm;
+}
+</style>

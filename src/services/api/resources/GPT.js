a4ccc4a920570c72f3c46c0cd88a08b58ef75533
aldemylla <aldemylla@gmail.com>
2024-06-03 11:24:38 -0300

    fix: Replaced gpt model

--- src/services/api/resources/GPT.js
+++ src/services/api/resources/GPT.js
@@ -10,18 +10,15 @@ const http = axios.create({
 export default {
   async getInsights(prompt) {
     const response = await http.post(env('VITE_GPT_URL'), {
-      input: {
-        prompt: `<s>[INST] ${prompt} [/INST]`,
-        sampling_params: {
-          max_tokens: 1024,
-          n: 1,
-          top_p: 0.95,
-          tok_k: 10,
-          temperature: 0.1,
-          do_sample: false,
+      messages: [
+        {
+          role: 'user',
+          content: `${prompt}`,
         },
-      },
+      ],
+      model: 'llama3-8b-8192',
     });
-    return response.data;
+
+    return response.data?.choices?.[0].message?.content;
   },
 };

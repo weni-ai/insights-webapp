8a4de9e103e41748143befdb73cd9ce098215bc9
mateuseduardomedeiros <eduardomedeirosrn2@gmail.com>
2024-06-05 18:03:19 -0300

    fix: change to graph_column

--- src/components/insights/widgets/DynamicWidget.vue
+++ src/components/insights/widgets/DynamicWidget.vue
@@ -90,7 +90,7 @@ export default {
 
       let { data } = this.widget.data;
 
-      if (this.widget.type === 'graph_bar') {
+      if (this.widget.type === 'graph_column') {
         data = sortByKey(data, 'label');
       }
 

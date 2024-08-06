1ba7ae2757369325be062b05c0179141973f9d9d
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-08-06 14:02:44 -0300

    fix: currency format

--- src/components/insights/drawers/DrawerConfigWidgetDynamic.vue
+++ src/components/insights/drawers/DrawerConfigWidgetDynamic.vue
@@ -214,7 +214,7 @@ export default {
               ? 'count'
               : config.result?.operation,
           filter: { flow: configuredFlow?.value },
-          currency: config.currency,
+          currency: !!config.result.currency,
           op_field: config.result?.name[0]?.value,
           ...operationRecurrenceConfigs,
         },

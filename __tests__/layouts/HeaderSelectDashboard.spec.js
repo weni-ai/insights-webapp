6c21ce5ff2dad85423e2a53d4496ffb77c429031
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-08-20 14:15:38 -0300

    fix: broken tests

--- __tests__/layouts/HeaderSelectDashboard.spec.js
+++ __tests__/layouts/HeaderSelectDashboard.spec.js
@@ -3,6 +3,8 @@ import { flushPromises, mount } from '@vue/test-utils';
 import { createRouter, createWebHistory } from 'vue-router';
 import { createStore } from 'vuex';
 import { routes } from '@/router';
+import dashboardsStore from '@/store/modules/dashboards';
+import refsStore from '@/store/modules/refs';
 
 import HeaderSelectDashboard from '@/components/insights/Layout/HeaderSelectDashboard/index.vue';
 
@@ -22,6 +24,10 @@ describe('HeaderSelectDashboard', () => {
             { name: 'Dashboard 2', uuid: '2' },
           ],
           currentDashboard: { name: 'Dashboard 1', uuid: '1' },
+          showDashboardConfig: false,
+        },
+        mutations: {
+          ...dashboardsStore.mutations,
         },
         getters: {
           dashboardDefault: () => ({ name: 'Default Dashboard' }),
@@ -34,23 +40,7 @@ describe('HeaderSelectDashboard', () => {
         },
       },
       refs: {
-        namespaced: true,
-        state: {
-          onboardingRefs: {
-            'select-dashboard': null,
-            'create-dashboard-button': null,
-            'widget-card-metric': null,
-            'widget-gallery': null,
-            'drawer-card-metric-config': null,
-            'widget-graph-funnel': null,
-            'drawer-graph-funnel': null,
-            'dashboard-onboarding-tour': null,
-            'widgets-onboarding-tour': null,
-          },
-          showCreateDashboardOnboarding: false,
-          showConfigWidgetOnboarding: false,
-          showCompleteOnboardingModal: false,
-        },
+        ...refsStore,
       },
     },
   });

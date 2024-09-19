7a51d1e08e8e61fb533587c61c771800047da288
Aldemylla <aldemylla@gmail.com>
2024-09-19 14:41:05 -0300

    feat: Styled the generate insight button and started the generated insight modal, along with generation animations

---
+++ src/components/insights/Layout/HeaderGenerateInsights/HeaderGenerateInsightButton.vue
@@ -0,0 +1,71 @@
+<template>
+  <button
+    class="header-generate-insight-button"
+    @click="handleGenerateInsightModal"
+  >
+    <img src="@/assets/images/shine.svg" />
+    {{ $t('insights_header.generate_insight.title') }}
+
+    <HeaderGenerateInsightModal
+      :show="isGenerateInsightModalOpen"
+      @close="handleGenerateInsightModal"
+    />
+  </button>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import HeaderGenerateInsightModal from './HeaderGenerateInsightModal.vue';
+
+export default {
+  name: 'HeaderGenerateInsightButton',
+
+  components: {
+    HeaderGenerateInsightModal,
+  },
+
+  data() {
+    return {
+      isGenerateInsightModalOpen: false,
+    };
+  },
+
+  computed: {
+    ...mapState({
+      token: (state) => state.config.token,
+    }),
+  },
+
+  methods: {
+    handleGenerateInsightModal() {
+      this.isGenerateInsightModalOpen = !this.isGenerateInsightModalOpen;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.header-generate-insight-button {
+  position: relative;
+
+  border: none;
+
+  display: flex;
+  gap: $unnnic-spacing-nano;
+  justify-content: center;
+  align-items: center;
+
+  padding: $unnnic-spacing-ant $unnnic-spacing-sm;
+
+  border-radius: $unnnic-border-radius-sm;
+  background: $unnnic-color-neutral-darkest;
+
+  color: $unnnic-color-weni-300;
+
+  font-family: $unnnic-font-family-secondary;
+  font-size: $unnnic-font-size-body-gt;
+
+  cursor: pointer;
+}
+</style>

b3af7f3f42ea45389c68a7225c9eeea652d821cd
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-26 10:40:46 -0300

    adding initial gradient logic

--- src/components/insights/Layout/HeaderGenerateInsights/HeaderGenerateInsightModal.vue
+++ src/components/insights/Layout/HeaderGenerateInsights/HeaderGenerateInsightModal.vue
@@ -22,8 +22,17 @@
         </button>
       </header>
 
-      <section class="header-generate-insight-modal__content">
+      <section
+        ref="content"
+        class="header-generate-insight-modal__content"
+        @scroll="checkScroll"
+      >
         <HeaderGenerateInsightText :text="generatedInsight" />
+
+        <div
+          v-if="showGradient"
+          class="gradient-overlay"
+        ></div>
 
         <footer
           v-if="generatedInsight"
@@ -60,6 +69,7 @@ export default {
   data() {
     return {
       generatedInsight: '',
+      showGradient: false,
     };
   },
 
@@ -68,7 +78,24 @@ export default {
       if (newShow && !this.generatedInsight) {
         this.generateInsight();
       }
+
+      if (newShow) {
+        this.$nextTick(() => {
+          this.checkScroll();
+        });
+      }
     },
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      this.checkScroll();
+    });
+    window.addEventListener('resize', this.checkScroll);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('resize', this.checkScroll);
   },
 
   methods: {
@@ -77,7 +104,17 @@ export default {
       setTimeout(() => {
         this.generatedInsight =
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dictum mauris volutpat, ornare quam vel, scelerisque est. Nulla nulla neque, scelerisque ac velit tincidunt, dapibus accumsan quam. Quisque aliquam nulla interdum arcu aliquet molestie. Curabitur lobortis maximus fringilla. Phasellus nec dolor efficitur, mattis ipsum eu, volutpat urna. In cursus, lacus nec semper sagittis, felis ante viverra ante, sit amet lacinia metus purus non diam. Vivamus quis fringilla leo. Proin mattis aliquet risus. <br/><br/> Vestibulum tincidunt, ipsum ut tempor luctus, sapien elit laoreet mi, quis ornare lacus urna quis erat. Duis et imperdiet massa. Nunc fringilla efficitur tellus, a cursus odio suscipit vel. Ut tempus metus elit, non lobortis nisi ultrices nec. Proin sem arcu, ultrices in rutrum ac, pulvinar sit amet magna. Pellentesque non lacus in lacus sollicitudin ultrices. Morbi commodo et lectus in scelerisque. Aliquam scelerisque nisi arcu, ut scelerisque tortor pulvinar vitae. Suspendisse tempor tincidunt turpis. Vestibulum vehicula ante at odio facilisis lacinia. <br/><br/> Integer ut diam feugiat, faucibus neque sit amet, pretium lectus. Quisque vel libero consequat est interdum lacinia. Proin volutpat, elit et lacinia tincidunt, est est ullamcorper neque, quis pharetra nisi lectus eu nisi. Donec diam urna, aliquam et pellentesque sed, congue sit amet ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras dictum mauris volutpat, ornare quam vel, scelerisque est. Nulla nulla neque, scelerisque ac velit tincidunt, dapibus accumsan quam. Quisque aliquam nulla interdum arcu aliquet molestie.';
+        this.checkScroll();
       }, 3000);
+    },
+
+    checkScroll() {
+      const content = this.$refs.content;
+      const scrollHeight = content.scrollHeight;
+      const clientHeight = content.clientHeight;
+
+      console.log('checkScroll', content, scrollHeight, clientHeight);
+      this.showGradient = scrollHeight > clientHeight;
     },
   },
 };
@@ -101,9 +138,7 @@ export default {
   z-index: 999;
 
   box-shadow: $unnnic-shadow-level-far;
-
   border-radius: $unnnic-border-radius-sm;
-
   padding: $unnnic-spacing-md;
 
   width: 35vw;
@@ -129,7 +164,6 @@ export default {
       gap: $unnnic-spacing-xs;
 
       color: $unnnic-color-weni-300;
-
       font-family: $unnnic-font-family-secondary;
       font-size: $unnnic-font-size-title-sm;
       font-weight: $unnnic-font-weight-bold;
@@ -163,6 +197,21 @@ export default {
         font-size: $unnnic-font-size-body-md;
       }
     }
+  }
+
+  .gradient-overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 9rem;
+    background: linear-gradient(
+      359deg,
+      #3b414d 0.54%,
+      rgba(59, 65, 77, 0.8) 62.61%,
+      rgba(59, 65, 77, 0) 91.82%
+    );
+    pointer-events: none;
   }
 }
 </style>

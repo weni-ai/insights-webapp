678fd5d3c8c68079071d9979d8196510fce63ef8
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-27 12:16:46 -0300

    adjusting service to match new logic

--- src/services/api/resources/GPT.js
+++ src/services/api/resources/GPT.js
@@ -1,38 +1,31 @@
-import axios from 'axios';
-import env from '@/utils/env';
-
 import { db as firebaseDB } from '@/utils/plugins/Firebase.js';
 import { collection, addDoc } from 'firebase/firestore';
-
-const http = axios.create({
-  headers: {
-    Authorization: env('VITE_GPT_AUTH'),
-  },
-});
+import http from '@/services/api/http';
+import Config from '@/store/modules/config';
 
 export default {
   async getInsights(prompt) {
-    const response = await http.post(env('VITE_GPT_URL'), {
-      messages: [
-        {
-          role: 'user',
-          content: `${prompt}`,
-        },
-      ],
-      model: 'llama3-8b-8192',
-    });
+    const { project } = Config.state;
+    const response = await http.post(
+      `/projects/${project.uuid}/sources/chat_completion/search/`,
+      {
+        prompt,
+      },
+    );
 
-    return response.data?.choices?.[0].message?.content;
+    return response;
   },
 
-  createReview({ user, helpful, comment }) {
-    addDoc(collection(firebaseDB, 'AI Reviews'), {
-      user,
-      helpful,
-      comment,
-      timestamp: new Date(),
-    }).catch((error) => {
+  async createReview({ user, helpful, comment }) {
+    try {
+      return await addDoc(collection(firebaseDB, 'AI Reviews'), {
+        user,
+        helpful,
+        comment,
+        timestamp: new Date(),
+      });
+    } catch (error) {
       console.error('Error writing AI Reviews document: ', error);
-    });
+    }
   },
 };

a43039f773ab1a5c91f98278d2430b26de7dd818
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-30 11:05:48 -0300

    adjusting envs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 export JSON_STRING='window.configs = { \
-  "VITE_GPT_URL":"'${VITE_GPT_URL}'", \
-  "VITE_GPT_AUTH":"'${VITE_GPT_AUTH}'", \
   "VITE_INSIGHTS_API_URL":"'${VITE_INSIGHTS_API_URL}'", \
+  "VITE_FIREBASE_CONFIG":"'${VITE_FIREBASE_CONFIG}'", \
   "VITE_HOTJAR_ID":"'${VITE_HOTJAR_ID}'", \
 }'
 sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html

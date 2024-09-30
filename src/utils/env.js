f68a16c369bb6ded35c785f16b5f077e6cb2f5f2
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-30 16:22:30 -0300

    adjusting utils .env logic

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,7 @@
 export default function env(name) {
+  if (import.meta.env[`VITE_${name}`] && name === 'VITE_FIREBASE_CONFIG')
+    return JSON.parse(import.meta.env[`VITE_${name}`]);
+
   return (
     window?.configs?.[name] ||
     import.meta.env[name] ||

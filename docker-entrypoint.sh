41a08bbaf73420aa5bcf388122aee38bc8ca37f1
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-30 14:37:50 -0300

    adjusting docker-entrypoint to fix insights

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+ESCAPED_FIREBASE_CONFIG=$(echo "${VITE_FIREBASE_CONFIG}" | sed 's/"/\\"/g')
+
 export JSON_STRING='window.configs = { \
   "VITE_INSIGHTS_API_URL":"'${VITE_INSIGHTS_API_URL}'", \
-  "VITE_FIREBASE_CONFIG":"'${VITE_FIREBASE_CONFIG}'", \
+  "VITE_FIREBASE_CONFIG":"'${ESCAPED_FIREBASE_CONFIG}'", \
   "VITE_HOTJAR_ID":"'${VITE_HOTJAR_ID}'", \
 }'
 sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/insights/index.html.tmpl > /tmp/index.html

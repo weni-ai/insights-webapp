d4bf205c82a74a06e9af1cb759d5b84c17e789bd
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-03 11:15:56 -0300

    adjusting HeaderGenerateInsightModal

--- src/components/insights/Layout/HeaderGenerateInsights/HeaderGenerateInsightModal.vue
+++ src/components/insights/Layout/HeaderGenerateInsights/HeaderGenerateInsightModal.vue
@@ -44,74 +44,18 @@
           class="gradient-overlay"
         ></section>
 
-        <footer
-          v-if="generatedInsight"
-          class="content__footer"
-        >
-          <p class="footer__description">
-            {{ $t('insights_header.generate_insight.by_ai') }}
-          </p>
-          <p
-            v-if="isFeedbackSent"
-            class="feedback_sent"
-          >
-            ✨{{ $t('insights_header.generate_insight.feedback.complete') }}
-          </p>
-          <section
-            v-if="isFeedbackSent"
-            ref="scrollTarget"
-          ></section>
-          <section
-            v-else-if="isRenderFooterFeedback"
-            class="footer__feedback"
-          >
-            <p class="footer__feedback__text">
-              {{ $t('insights_header.generate_insight.feedback.question') }}
-            </p>
-            <section class="footer__feedback__container__btns">
-              <UnnnicButton
-                type="tertiary"
-                iconLeft="thumb_up"
-                :text="$t('insights_header.generate_insight.button.yes')"
-                :class="[
-                  'footer__feedback__btn',
-                  { 'footer__feedback__btn-active': isBtnYesActive },
-                ]"
-                @click.stop="handlePositiveFeedback"
-              />
-              <UnnnicButton
-                type="tertiary"
-                iconLeft="thumb_down"
-                :text="$t('insights_header.generate_insight.button.no')"
-                :class="[
-                  'footer__feedback__btn',
-                  { 'footer__feedback__btn-active': isBtnNoActive },
-                ]"
-                @click.stop="handleNegativeFeedback"
-              />
-            </section>
-            <section
-              v-if="isBtnNoActive || isBtnYesActive"
-              class="footer__feedback__container__area"
-            >
-              <textarea
-                v-model="feedbackText"
-                rows="7"
-                class="footer__feedback__textarea"
-                :placeholder="handlePlaceholderTextArea()"
-                :disabled="isSubmitFeedbackLoading"
-              />
-              <UnnnicButton
-                type="tertiary"
-                :text="$t('insights_header.generate_insight.button.send')"
-                class="footer__feedback__btn_send"
-                :disabled="isSubmitFeedbackLoading"
-                :loading="isSubmitFeedbackLoading"
-                @click="submitReview"
-              />
-            </section>
-          </section>
-        </footer>
+        <InsightModalFooter
+          :generatedInsight="generatedInsight"
+          :isFeedbackSent="isFeedbackSent"
+          :isRenderFooterFeedback="isRenderFooterFeedback"
+          :isBtnYesActive="isBtnYesActive"
+          :isBtnNoActive="isBtnNoActive"
+          :feedbackText="feedbackText"
+          :isSubmitFeedbackLoading="isSubmitFeedbackLoading"
+          @handle-positive-feedback="handlePositiveFeedback"
+          @handle-negative-feedback="handleNegativeFeedback"
+          @submit-review="submitReview"
+        />
       </section>
     </section>
   </Transition>
@@ -119,6 +63,7 @@
 
 <script>
 import HeaderGenerateInsightText from './HeaderGenerateInsightText.vue';
+import InsightModalFooter from './InsightModalFooter.vue';
 import firebaseService from '@/services/api/resources/GPT';
 import mitt from 'mitt';
 import { formatSecondsToHumanString } from '@/utils/time';
@@ -130,6 +75,7 @@ export default {
 
   components: {
     HeaderGenerateInsightText,
+    InsightModalFooter,
   },
 
   props: {
@@ -178,13 +124,6 @@ export default {
         });
       }
     },
-    isFeedbackSent(newValue) {
-      if (newValue) {
-        this.$nextTick(() => {
-          this.scrollToEnd();
-        });
-      }
-    },
   },
 
   mounted() {
@@ -202,11 +141,6 @@ export default {
   methods: {
     handleTypingComplete() {
       this.isRenderFeedback = true;
-    },
-    scrollToEnd() {
-      if (this.$refs.scrollTarget) {
-        this.$refs.scrollTarget.scrollIntoView({ behavior: 'smooth' });
-      }
     },
     async submitReview() {
       this.isSubmitFeedbackLoading = true;
@@ -412,142 +346,6 @@ export default {
     &::-webkit-scrollbar {
       width: 0;
     }
-
-    .content__footer {
-      display: flex;
-      flex-direction: column;
-      justify-content: end;
-      max-height: 100%;
-
-      .footer__description {
-        color: $unnnic-color-neutral-clean;
-        text-align: right;
-        font-size: $unnnic-font-size-body-md;
-      }
-
-      .feedback_sent {
-        text-align: start;
-        color: $unnnic-color-neutral-white;
-
-        font-family: $unnnic-font-family-secondary;
-        font-size: $unnnic-font-size-body-lg;
-        font-style: normal;
-        font-weight: $unnnic-font-weight-bold;
-      }
-
-      .footer__feedback {
-        display: flex;
-        flex-direction: column;
-        overflow-y: auto;
-        max-height: 100%;
-        margin-top: $unnnic-spacing-sm;
-        gap: $unnnic-spacing-sm;
-
-        &::-webkit-scrollbar {
-          width: 0;
-        }
-
-        &__text {
-          display: flex;
-          align-items: flex-start;
-          font-family: $unnnic-font-family-secondary;
-          color: $unnnic-color-neutral-clean;
-          font-weight: $unnnic-font-weight-regular;
-          font-size: $unnnic-font-size-body-gt;
-        }
-
-        &__container__btns {
-          display: flex;
-          align-items: flex-start;
-          gap: $unnnic-spacing-ant;
-        }
-
-        &__container__area {
-          display: flex;
-          flex-direction: column;
-          gap: $unnnic-spacing-sm;
-        }
-
-        &__btn {
-          border-radius: $unnnic-border-radius-sm;
-          border: 1px solid $unnnic-color-neutral-dark;
-          background: $unnnic-color-neutral-darkest;
-          color: $unnnic-color-neutral-clean;
-          :deep(.material-symbols-rounded.unnnic-icon-scheme--neutral-dark) {
-            color: $unnnic-color-neutral-clean;
-          }
-
-          &:hover {
-            border: 1px solid $unnnic-color-neutral-cloudy;
-            background-color: inherit;
-          }
-
-          &-active {
-            color: $unnnic-color-neutral-cleanest;
-            :deep(.material-symbols-rounded.unnnic-icon-scheme--neutral-dark) {
-              color: $unnnic-color-neutral-cleanest;
-              font-family: Material Symbols Rounded Filled;
-            }
-          }
-        }
-
-        &__textarea {
-          border-radius: $unnnic-border-radius-sm;
-          border: 1px solid $unnnic-color-neutral-dark;
-          background: $unnnic-color-neutral-darkest;
-          display: flex;
-          padding: $unnnic-spacing-ant $unnnic-spacing-sm;
-          align-items: flex-start;
-          align-self: stretch;
-
-          &::placeholder {
-            color: $unnnic-color-neutral-cloudy;
-          }
-
-          color: $unnnic-color-neutral-clean;
-
-          font-family: $unnnic-font-family-secondary;
-          font-size: $unnnic-font-size-body-gt;
-          font-style: normal;
-          font-weight: $unnnic-font-weight-regular;
-
-          &:focus {
-            border-radius: 0.25rem;
-            border: 1px solid $unnnic-color-neutral-cloudy;
-            background: $unnnic-color-neutral-darkest;
-          }
-
-          &:focus-visible {
-            outline: none;
-            outline-offset: 0;
-          }
-        }
-
-        &__btn_send {
-          display: flex;
-          padding: $unnnic-spacing-ant $unnnic-spacing-sm;
-          justify-content: center;
-          align-items: center;
-          gap: $unnnic-spacing-nano;
-
-          border-radius: $unnnic-border-radius-sm;
-          border: 1px solid $unnnic-color-neutral-cloudy;
-          background: $unnnic-color-neutral-dark;
-          color: $unnnic-color-neutral-cleanest;
-
-          &:hover {
-            border: 1px solid $unnnic-color-neutral-clean;
-            background: $unnnic-color-neutral-cloudy;
-          }
-
-          &:disabled {
-            color: $unnnic-color-neutral-cloudy;
-            border: 1px solid $unnnic-color-neutral-dark;
-            background: $unnnic-color-neutral-darkest;
-          }
-        }
-      }
-    }
   }
 
   .gradient-overlay {
@@ -555,7 +353,7 @@ export default {
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 7rem;
+    height: 9rem;
     background: linear-gradient(
       359deg,
       #3b414d 0.54%,

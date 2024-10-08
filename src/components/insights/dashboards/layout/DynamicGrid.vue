8c56698df2e398e4ca7ac1fece5cb61187f2db04
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-08 16:39:42 -0300

    creating DynamicGrid

---
+++ src/components/insights/dashboards/layout/DynamicGrid.vue
@@ -0,0 +1,103 @@
+<template>
+  <div :class="gridLayoutClass">
+    <div
+      v-for="(_, index) in boxes"
+      :key="index"
+      :class="getBoxClass(index)"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  numberCards: {
+    type: Number,
+    required: true,
+    validator: (value: number) => [0, 1, 2, 3].includes(value),
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const gridLayoutClass = computed(() => {
+  return 'grid_layout layout-' + props.numberCards;
+});
+
+const boxes = computed(() => {
+  switch (props.numberCards) {
+    case 0:
+      return new Array(9).fill(null);
+    case 1:
+      return new Array(7).fill(null);
+    case 2:
+      return new Array(5).fill(null);
+    case 3:
+      return new Array(3).fill(null);
+    default:
+      return [];
+  }
+});
+
+const getBoxClass = (index: number) => {
+  const defaultClass = props.active ? 'box box-active' : 'box';
+
+  switch (props.numberCards) {
+    case 0:
+      return defaultClass;
+    case 1:
+      return index === 2 ? `${defaultClass} box-filled box-3` : defaultClass;
+    case 2:
+      return index === 3
+        ? `${defaultClass} box-filled box-2`
+        : index === 4
+          ? `${defaultClass} box-filled box-3`
+          : defaultClass;
+    case 3:
+      return index === 0
+        ? `${defaultClass} box-filled box-1`
+        : index === 1
+          ? `${defaultClass} box-filled box-2`
+          : `${defaultClass} box-filled box-3`;
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.grid_layout {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 0.125rem;
+  height: 100%;
+  width: 100%;
+}
+
+.box {
+  border-radius: 0.1025rem;
+  background-color: $unnnic-color-neutral-cleanest;
+
+  &-active {
+    background-color: $unnnic-color-weni-600;
+  }
+}
+
+.box-filled {
+  grid-row: 1 / 4;
+}
+
+.box-1 {
+  grid-column: 1;
+}
+
+.box-2 {
+  grid-column: 2;
+}
+
+.box-3 {
+  grid-column: 3;
+}
+</style>

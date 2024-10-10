ae4cc0445ffc9b35223753a79428f701532a107f
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-10 19:36:31 -0300

    revert FilterDate logic

--- src/components/insights/Layout/HeaderFilters/FilterDate.vue
+++ src/components/insights/Layout/HeaderFilters/FilterDate.vue
@@ -1,106 +1,44 @@
 <template>
-  <UnnnicSelectSmart
-    v-model="currentDate"
-    :options="dates"
-    size="md"
+  <UnnnicInputDatePicker
     class="filter-date"
-    @update:model-value="updateCurrentDate"
+    :modelValue="modelValue"
+    size="sm"
+    :inputFormat="$t('date_format')"
+    position="right"
+    @update:model-value="$emit('update:modelValue', $event)"
   />
 </template>
 
-<script setup>
-import {
-  getYesterdayDate,
-  getLastNDays,
-  getLastMonthRange,
-} from '@/utils/time';
-import { defineProps, defineEmits, computed, ref } from 'vue';
-import i18n from '@/utils/plugins/i18n';
-
-const props = defineProps({
-  modelValue: {
-    type: Object,
-    default: () => ({}),
+<script>
+export default {
+  name: 'FilterDate',
+  props: {
+    modelValue: {
+      type: Object,
+      default: () => {},
+    },
   },
-});
-
-const emit = defineEmits(['update:modelValue']);
-
-const getDateRanges = () => ({
-  yesterday: getYesterdayDate(),
-  last7Days: getLastNDays(7),
-  last14Days: getLastNDays(14),
-  last30Days: getLastNDays(30),
-  last90Days: getLastNDays(90),
-  lastMonth: getLastMonthRange(),
-});
-
-const dateRanges = getDateRanges();
-
-const currentDate = ref([props.modelValue]);
-
-const dates = computed(() => [
-  createDateOption(
-    i18n.global.t('select_date.yesterday', {
-      date: dateRanges.yesterday.dmFormat,
-    }),
-    dateRanges.yesterday,
-  ),
-  ...[7, 14, 30, 90].map((days) =>
-    createDateOption(
-      i18n.global.t('select_date.last_days', {
-        value: days,
-        date: dateRanges[`last${days}Days`].dmFormat,
-      }),
-      dateRanges[`last${days}Days`],
-    ),
-  ),
-  createDateOption(
-    i18n.global.t('select_date.previous_month', {
-      date: dateRanges.lastMonth.dmFormat,
-    }),
-    dateRanges.lastMonth,
-  ),
-]);
-
-const createDateOption = (label, range) => ({
-  label,
-  value: {
-    start: range.start,
-    end: range.end,
-  },
-});
-
-const updateCurrentDate = (option) => {
-  currentDate.value = option;
-  emit('update:modelValue', option[0].value);
+  emits: ['update:modelValue'],
 };
 </script>
 
 <style lang="scss" scoped>
-.filter-date {
-  :deep(.input) {
-    color: $unnnic-color-neutral-darkest;
+// .dropdown class comes from the unnnic component and is used here to override its style
+.filter-date.dropdown {
+  display: grid;
+  height: 100%;
 
-    font-family: $unnnic-font-family-secondary;
-    font-size: $unnnic-font-size-body-gt;
-    font-style: normal;
-    font-weight: $unnnic-font-weight-regular;
-    line-height: $unnnic-line-height-large * 1.375;
-  }
+  :deep(.unnnic-form-input) {
+    height: 100%;
 
-  :deep(.unnnic-select-smart-option--selectable) {
-    &:hover {
-      color: $unnnic-color-weni-600;
-      font-weight: $unnnic-font-weight-bold;
+    &.text-input > .unnnic-icon {
+      transform: translateY(-50%);
+      top: 50%;
     }
 
-    color: $unnnic-color-neutral-darkest;
-    font-family: $unnnic-font-family-secondary;
-    font-size: $unnnic-font-size-body-gt;
-    font-style: normal;
-    font-weight: $unnnic-font-weight-regular;
-    line-height: $unnnic-line-height-large * 1.375;
+    .input {
+      font-size: $unnnic-font-size-body-gt;
+    }
   }
 }
 </style>

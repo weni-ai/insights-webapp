e2fde2f3570ca661a3ec9021cdc3b1727d6a8df6
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-09 23:10:56 -0300

    adding new logic to FilterDate component

--- src/components/insights/Layout/HeaderFilters/FilterDate.vue
+++ src/components/insights/Layout/HeaderFilters/FilterDate.vue
@@ -1,24 +1,78 @@
 <template>
-  <UnnnicInputDatePicker
-    class="filter-date"
-    :modelValue="modelValue"
-    size="sm"
-    :inputFormat="$t('date_format')"
-    position="right"
-    @update:model-value="$emit('update:modelValue', $event)"
+  <UnnnicSelectSmart
+    v-model="currentDate"
+    :options="dates"
+    size="md"
+    @update:model-value="updateCurrentDate"
   />
 </template>
 
-<script>
-export default {
-  name: 'FilterDate',
-  props: {
-    modelValue: {
-      type: Object,
-      default: () => {},
-    },
+<script setup>
+import {
+  getYesterdayDate,
+  getLastNDays,
+  getLastMonthRange,
+} from '@/utils/time';
+import { defineProps, defineEmits, computed, ref } from 'vue';
+import i18n from '@/utils/plugins/i18n';
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: () => ({}),
   },
-  emits: ['update:modelValue'],
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const getDateRanges = () => ({
+  yesterday: getYesterdayDate(),
+  last7Days: getLastNDays(7),
+  last14Days: getLastNDays(14),
+  last30Days: getLastNDays(30),
+  last90Days: getLastNDays(90),
+  lastMonth: getLastMonthRange(),
+});
+
+const dateRanges = getDateRanges();
+
+const currentDate = ref([props.modelValue]);
+
+const dates = computed(() => [
+  createDateOption(
+    i18n.global.t('select_date.yesterday', {
+      date: dateRanges.yesterday.dmFormat,
+    }),
+    dateRanges.yesterday,
+  ),
+  ...[7, 14, 30, 90].map((days) =>
+    createDateOption(
+      i18n.global.t('select_date.last_days', {
+        value: days,
+        date: dateRanges[`last${days}Days`].dmFormat,
+      }),
+      dateRanges[`last${days}Days`],
+    ),
+  ),
+  createDateOption(
+    i18n.global.t('select_date.previous_month', {
+      date: dateRanges.lastMonth.dmFormat,
+    }),
+    dateRanges.lastMonth,
+  ),
+]);
+
+const createDateOption = (label, range) => ({
+  label,
+  value: {
+    start: range.start,
+    end: range.end,
+  },
+});
+
+const updateCurrentDate = (option) => {
+  currentDate.value = option;
+  emit('update:modelValue', option[0].value);
 };
 </script>
 

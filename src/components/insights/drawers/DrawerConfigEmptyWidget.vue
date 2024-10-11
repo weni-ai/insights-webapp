1375a885afd69340f4128ecb81c7de3bfd7b129d
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-11 17:58:45 -0300

    adding new Drawer

---
+++ src/components/insights/drawers/DrawerConfigEmptyWidget.vue
@@ -0,0 +1,152 @@
+<template>
+  <section>
+    <UnnnicLabel :label="$t('drawers.config_card.name_card.label')" />
+    <UnnnicInput
+      v-model="config.name"
+      :placeholder="$t('drawers.config_card.name_card.placeholder')"
+    />
+  </section>
+
+  <component
+    :is="currentFormComponent"
+    v-on="currentFormEvents"
+  />
+
+  <SelectEmojiButton
+    v-model="config.friendly_id"
+    :pickerPosition="type === 'executions' ? 'bottom' : 'top'"
+  />
+
+  <UnnnicButton
+    :text="$t('drawers.reset_widget')"
+    type="tertiary"
+    :disabled="disableResetWidgetButton"
+    @click="$emit('reset-widget')"
+  />
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+import { checkDeepEmptyValues } from '@/utils/object';
+
+import FormExecutions from './DrawerForms/Card/FormExecutions.vue';
+import FormFlowResult from './DrawerForms/Card/FormFlowResult.vue';
+import FormDataCrossing from './DrawerForms/Card/FormDataCrossing/index.vue';
+import SelectEmojiButton from '@/components/SelectEmojiButton.vue';
+
+export default {
+  name: 'DrawerConfigEmptyWidget',
+
+  components: {
+    SelectEmojiButton,
+  },
+
+  props: {
+    type: {
+      type: String,
+      default: '',
+      validate(value) {
+        return ['executions', 'flow_result', 'data_crossing'].includes(value);
+      },
+    },
+  },
+
+  emits: ['update-disable-primary-button', 'reset-widget'],
+
+  data() {
+    return {
+      initialConfigStringfy: '',
+      config: null,
+      isCurrentFormValid: false,
+    };
+  },
+
+  computed: {
+    ...mapState({
+      widgetConfig: (state) => state.widgets.currentWidgetEditing.config,
+    }),
+
+    currentFormComponent() {
+      const componentMap = {
+        executions: FormExecutions,
+        flow_result: FormFlowResult,
+        data_crossing: FormDataCrossing,
+      };
+
+      return componentMap[this.type] || null;
+    },
+
+    currentFormEvents() {
+      const defaultEvents = {
+        'update:is-valid-form': (isValid) =>
+          (this.isCurrentFormValid = !!isValid),
+      };
+
+      const mappingEvents = {};
+
+      return { ...defaultEvents, ...mappingEvents[this.type] };
+    },
+
+    isAllFieldsValid() {
+      return this.config?.name && this.isCurrentFormValid;
+    },
+
+    disableResetWidgetButton() {
+      return checkDeepEmptyValues(this.widgetConfig);
+    },
+  },
+
+  watch: {
+    config: {
+      deep: true,
+      handler(newConfig) {
+        this.updateCurrentWidgetEditingConfig({
+          ...this.widgetConfig,
+          ...newConfig,
+        });
+      },
+    },
+
+    widgetConfig: {
+      deep: true,
+      handler() {
+        this.updatePrimaryButtonState();
+      },
+    },
+
+    isAllFieldsValid() {
+      this.updatePrimaryButtonState();
+    },
+  },
+
+  created() {
+    this.config = {
+      ...this.widgetConfig,
+      type: this.type,
+      friendly_id: this.widgetConfig.friendly_id || '',
+    };
+    this.initializeConfigString();
+  },
+
+  methods: {
+    ...mapActions({
+      updateCurrentWidgetEditingConfig:
+        'widgets/updateCurrentWidgetEditingConfig',
+    }),
+
+    initializeConfigString() {
+      if (this.config && !this.initialConfigStringfy) {
+        this.initialConfigStringfy = JSON.stringify(this.widgetConfig);
+      }
+    },
+    updatePrimaryButtonState() {
+      const disablePrimaryButton =
+        this.initialConfigStringfy === JSON.stringify(this.widgetConfig) ||
+        !this.isAllFieldsValid;
+
+      this.$emit('update-disable-primary-button', disablePrimaryButton);
+    },
+  },
+};
+</script>

b18dcda08042fb1509be85461a36265151dd8013
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-31 18:20:52 -0300

    adjusting dynamic data to render correct data

--- src/components/insights/widgets/DynamicWidget.vue
+++ src/components/insights/widgets/DynamicWidget.vue
@@ -140,6 +140,7 @@ export default {
     widgetVtexData() {
       if (this.widget.type === 'vtex_order' && this.widget.data) {
         const { total_value, average_ticket, orders } = this.widget.data;
+        const existOrders = orders !== '';
         const existTotalValue = total_value !== '';
         const existAverageTicketValue = average_ticket !== '';
         const currentSymbol =
@@ -150,7 +151,9 @@ export default {
 
         return {
           ...this.widget.data,
-          orders: (orders || 0).toLocaleString(this.$i18n.locale || 'en-US'),
+          orders: existOrders
+            ? (orders || 0).toLocaleString(this.$i18n.locale || 'en-US')
+            : orders,
           total_value: existTotalValue
             ? numbersNormalization(total_value)
             : total_value,

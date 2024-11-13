9f8c8cdf29cc62d62d23e3be7a3ca393f459f385
Marcus Vinícius <marcusvinici25@gmail.com>
2024-11-13 18:17:47 -0300

    adding DrawerConfigContentRecurrence init logic

---
+++ src/components/insights/drawers/DrawerConfigContentRecurrence.vue
@@ -0,0 +1,130 @@
+<template>
+  <section class="content-recurrence">
+    <section>
+      <UnnnicLabel :label="$t('drawers.config_card.name_card.label')" />
+      <UnnnicInput
+        v-model="config.name"
+        :placeholder="$t('drawers.config_card.name_card.placeholder')"
+      />
+    </section>
+
+    <SelectFlow
+      v-model="config.flow.uuid"
+      data-test-id="select-flow"
+    />
+
+    <SelectFlowResult
+      v-model="config.flow.result"
+      data-test-id="select-flow-result"
+      :flow="config.flow?.uuid"
+      :disabled="!config.flow?.uuid"
+    />
+    <UnnnicButton
+      class="clear-button"
+      :text="$t('drawers.reset_widget')"
+      type="tertiary"
+      :disabled="isDisableResetWidget"
+      @click="resetWidget"
+    />
+  </section>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+import SelectFlow from '@/components/SelectFlow.vue';
+import SelectFlowResult from '@/components/SelectFlowResult.vue';
+
+export default {
+  name: 'DrawerConfigContentRecurrence',
+
+  components: {
+    SelectFlow,
+    SelectFlowResult,
+  },
+
+  emits: ['update:is-valid-form', 'reset-widget'],
+
+  data() {
+    return {
+      config: null,
+    };
+  },
+
+  computed: {
+    ...mapState({
+      widgetConfig: (state) => state.widgets.currentWidgetEditing.config,
+    }),
+
+    isValidForm() {
+      const { config } = this;
+
+      return config?.flow.uuid && config?.flow.result && config?.operation;
+    },
+
+    isDisableResetWidget() {
+      return false;
+    },
+  },
+
+  watch: {
+    config: {
+      deep: true,
+      handler(newConfig) {
+        this.updateCurrentWidgetEditingConfig({
+          ...this.widgetConfig,
+          ...newConfig,
+        });
+
+        if (newConfig?.operation === 'recurrence') this.config.currency = false;
+      },
+    },
+
+    'config.flow?.uuid'(_newFlow, oldFlow) {
+      if (typeof oldFlow === 'object') {
+        this.config.flow.result = '';
+      }
+    },
+
+    isValidForm: {
+      immediate: true,
+      handler(newIsValidForm) {
+        this.$emit('update:is-valid-form', newIsValidForm);
+      },
+    },
+  },
+
+  created() {
+    const { widgetConfig } = this;
+    this.config = {
+      flow: {
+        uuid: widgetConfig.flow?.uuid || '',
+        result: widgetConfig.flow?.result || '',
+      },
+      operation: widgetConfig.operation || '',
+      currency: widgetConfig.currency || false,
+    };
+  },
+
+  methods: {
+    ...mapActions({
+      updateCurrentWidgetEditingConfig:
+        'widgets/updateCurrentWidgetEditingConfig',
+    }),
+    resetWidget() {
+      this.$emit('reset-widget');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.content-recurrence {
+  display: grid;
+  gap: $unnnic-spacing-nano;
+
+  .clear-button {
+    margin-top: $unnnic-spacing-nano;
+  }
+}
+</style>

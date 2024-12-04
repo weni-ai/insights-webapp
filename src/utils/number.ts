f9357ab694a8849317dad81cd62291398f3502b6
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-12-04 11:42:51 -0300

    fix: change number utils to ts

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,6 +1,10 @@
 import i18n from './plugins/i18n';
 
-export function getPercentageOf(val, total, precision = 2) {
+export function getPercentageOf(
+  val: number,
+  total: number,
+  precision: number = 2,
+) {
   const percentage = (val / total) * 100;
   return percentage.toLocaleString(i18n.global.locale, {
     maximumFractionDigits: precision,

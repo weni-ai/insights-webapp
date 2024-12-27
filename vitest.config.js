b89b42d3b1de83673ca74da42a6fb7a7e3e84717
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-12-27 11:52:14 -0300

    wip: remove api files from coverage

--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,13 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
+      exclude: [
+        ...configDefaults.exclude,
+        'e2e/*',
+        'src/router/**',
+        'src/services/api/http.js',
+        'src/services/api/customError.js',
+      ],
       root: fileURLToPath(new URL('./', import.meta.url)),
       globals: true,
       setupFiles: './setupVitest.js',
@@ -16,7 +22,13 @@ export default mergeConfig(
         reporter: ['text', 'json', 'html'],
         reportsDirectory: './coverage',
         include: ['src/**/*.{vue,js,ts}'],
-        exclude: ['src/main.js', '**/__tests__/**'],
+        exclude: [
+          'src/main.js',
+          '**/__tests__/**',
+          'src/router/**',
+          'src/services/api/http.js',
+          'src/services/api/customError.js',
+        ],
       },
     },
   }),

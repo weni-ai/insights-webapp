0c2ed5efab15aac031c03dc5b314af0b460eb95d
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-16 12:00:11 -0300

    change VITE variables

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,8 +1,8 @@
 export default function env(name) {
   const envData = process.env?.[name] || window?.configs?.[name] || import.meta.env[name];
 
-  if (name === 'FIREBASE_CONFIG')
+  if (name === 'FIREBASE_CONFIG' && typeof envData === 'string')
     return JSON.parse(envData);
-  
+
   return envData
 }

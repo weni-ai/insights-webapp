9f805b78d3d28c83377aeeb858b6ca3c534d7115
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-29 16:50:37 -0300

    adding logic to seeMore

--- src/components/insights/widgets/DynamicWidget.vue
+++ src/components/insights/widgets/DynamicWidget.vue
@@ -138,7 +138,7 @@ export default {
         recurrence: {
           widget: this.widget,
           data: this.widget?.data || [],
-          seeMore: !!report,
+          seeMore: !!report && this.widget?.data,
         },
       };
 

6e34429aaecb3ee4a85cd1cc8e60d55e719aa76d
Marcus Vinícius <marcusvinici25@gmail.com>
2025-02-13 23:49:39 -0300

    Adjusting DynamicDashboard to support expansive logic

--- src/views/insights/DynamicDashboard.vue
+++ src/views/insights/DynamicDashboard.vue
@@ -9,40 +9,70 @@
 <script lang="ts" setup>
 import { computed, watch } from 'vue';
 import { useStore } from 'vuex';
-import DashboardCustom from './DashboardCustom.vue';
+import DashboardCustom from '../../components/insights/dashboards/DashboardCustom.vue';
+import ExpansiveWidget from '@/components/insights/widgets/ExpansiveWidget.vue';
 
 const store = useStore();
 
-const dashboardType = computed(() => {
-  console.log('dashboards', store.state.dashboards.currentDashboard);
-  const dashboardTypes = {};
+const currentExpansiveWidget = computed(() => {
+  return store.state.widgets?.currentExpansiveWidget;
+});
 
-  return 'custom_dashboard';
+const dashboardType = computed(() => {
+  const type = currentExpansiveWidget.value ? 'expansive' : 'custom';
+
+  const dashboardTypes = {
+    custom: 'custom_dashboard',
+    expansive: 'expansive_widget',
+  };
+
+  return dashboardTypes[type] || dashboardTypes['custom'];
 });
 
 const currentComponent = computed(() => {
   const componentMap = {
     custom_dashboard: DashboardCustom,
+    expansive_widget: ExpansiveWidget,
+    template_dashboard: null,
   };
 
   return componentMap[dashboardType.value] || null;
 });
+
+const dashboardProps = computed(() => {
+  const mappingProps = {
+    expansive_widget: {
+      widget: currentExpansiveWidget,
+    },
+  };
+
+  return mappingProps[dashboardType.value] || null;
+});
+
+const dashboardEvents = computed(() => {
+  return null;
+});
+
+const currentDashboardUuid = computed(
+  () => store.state.dashboards.currentDashboard?.uuid,
+);
 
 const getCurrentDashboardWidgets = () => {
   return store.dispatch('widgets/getCurrentDashboardWidgets');
 };
 
 const resetCurrentDashboardWidgets = () => {
-  return store.dispatch('widgets/RESET_CURRENT_DASHBOARD_WIDGETS');
+  return store.commit('widgets/RESET_CURRENT_DASHBOARD_WIDGETS');
 };
 
 watch(
-  () => store.state.dashboards.currentDashboard.uuid,
-  (newCurrentDashboardUuid) => {
+  currentDashboardUuid,
+  async (newCurrentDashboardUuid) => {
     if (newCurrentDashboardUuid) {
       resetCurrentDashboardWidgets();
       getCurrentDashboardWidgets();
     }
   },
+  { immediate: true },
 );
 </script>

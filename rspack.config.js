e81d9371f5f15e758605c20faf72133f3d1d8c89
Marcus Vinícius <marcusvinici25@gmail.com>
2025-02-28 16:31:27 -0300

    adding debug to verify rspack

--- rspack.config.js
+++ rspack.config.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // Target browsers, see: https://github.com/browserslist/browserslist
 const targets = ['chrome >= 87', 'edge >= 88', 'firefox >= 78', 'safari >= 14'];
-
+console.log('process.env =======================>', process.env);
 module.exports = defineConfig({
   context: __dirname,
   devServer: {

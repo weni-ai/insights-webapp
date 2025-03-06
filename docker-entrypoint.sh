1593e33d6a7de52ccba057b82a7ea8ca7907bef7
Marcus Vinícius <marcusvinici25@gmail.com>
2025-03-06 18:33:05 -0300

    revert docker-entrypoints.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ ESCAPED_FIREBASE_CONFIG=$(echo "${FIREBASE_CONFIG}" | sed 's/\//\\\//g')
 
 export JSON_STRING='window.configs = { \
   "INSIGHTS_API_URL": "'${INSIGHTS_API_URL}'", \
-  "FIREBASE_CONFIG": "'${ESCAPED_FIREBASE_CONFIG}'", \
+  "FIREBASE_CONFIG": '${ESCAPED_FIREBASE_CONFIG}', \
   "HOTJAR_ID": "'${HOTJAR_ID}'", \
   "ENVIRONMENT": "'${ENVIRONMENT}'", \
   "SENTRY_DSN": "'${SENTRY_DSN}'", \

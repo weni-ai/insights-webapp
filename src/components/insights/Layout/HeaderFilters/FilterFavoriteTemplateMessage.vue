5e296be02b0510ef5a15941faf4233c7f01ce48a
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2025-03-07 11:46:40 -0300

    feat: init select template logic

--- src/components/insights/Layout/HeaderFilters/FilterFavoriteTemplateMessage.vue
+++ src/components/insights/Layout/HeaderFilters/FilterFavoriteTemplateMessage.vue
@@ -1,23 +1,24 @@
 <template>
   <section
-    v-if="currentDashboard?.name === 'test-meta-templates-message'"
+    v-if="currentDashboard?.config?.is_whatsapp_integration"
     class="filter-favorite-template"
   >
     <p class="filter-favorite-template__label">
       {{ `${$t('template_messages_dashboard.favorites')}:` }}
     </p>
     <UnnnicSelectSmart
-      v-model="selectedFavorite"
+      :modelValue="selectedFavorite"
       type="secondary"
       :options="favoriteOptions"
       autocomplete
       autocompleteClearOnFocus
+      @update:model-value="selectFavorite"
     />
   </section>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 import i18n from '@/utils/plugins/i18n';
 
@@ -27,13 +28,42 @@ const currentDashboard = computed(
   () => store.state.dashboards.currentDashboard,
 );
 
+const favorites = computed(
+  () => store.state.metaTemplateMessage.favoritesTemplates,
+);
+
+const selectedTemplateUuid = computed(
+  () => store.state.metaTemplateMessage.selectedTemplateUuid,
+);
+
 const favoriteOptions = computed(() => {
   const options = [{ label: i18n.global.t('select'), value: '' }];
+
+  favorites.value.forEach((favorite) =>
+    options.push({ label: favorite.name, value: favorite.id }),
+  );
 
   return options;
 });
 
-const selectedFavorite = ref([]);
+const selectedFavorite = computed(
+  () => store.state.metaTemplateMessage.selectedFavoriteTemplate,
+);
+
+const selectFavorite = (favorite) => {
+  store.dispatch('metaTemplateMessage/setSelectedFavorite', favorite);
+};
+
+onMounted(async () => {
+  await store.dispatch('metaTemplateMessage/getFavoritesTemplates');
+});
+
+watch(selectedTemplateUuid, (newUuid, oldUuid) => {
+  const favoritesIds = favorites.value.map((favorite) => favorite.id);
+  const newSelectedIsFavorite = favoritesIds.includes(newUuid);
+  if (!oldUuid || newSelectedIsFavorite) return;
+  selectFavorite(favoriteOptions.value[0]);
+});
 </script>
 
 <style lang="scss" scoped>

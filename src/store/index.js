6854f35be1864d8b6501c685cb4b111345c00944
Marcus Vinícius <marcusvinici25@gmail.com>
2025-03-27 15:43:20 -0300

    adding new store to columnsFilter

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,7 @@ import onboarding from './modules/onboarding';
 import user from './modules/user';
 import gpt from './modules/gpt';
 import metaTemplateMessage from './modules/templates/metaTemplateMessage';
+import agentsColumnsFilter from './modules/agentsColumnsFilter';
 
 const store = createStore({
   modules: {
@@ -20,6 +21,7 @@ const store = createStore({
     user,
     gpt,
     metaTemplateMessage,
+    agentsColumnsFilter,
   },
 });
 

d2b12694ab5bf1ce2ae961393c44565a23ae3c00
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2025-04-03 17:15:25 -0300

    wip: reset to main

---
+++ src/components/insights/widgets/SingleTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="single-table">
+    <h1
+      v-if="props.title"
+      class="single-table__title"
+      data-testid="single-table-title"
+    >
+      {{ title }}
+    </h1>
+    <UnnnicTableNext
+      data-testid="single-table"
+      class="single-table__table"
+      :pagination="pagination"
+      :paginationTotal="paginationTotal"
+      :paginationInterval="paginationInterval"
+      :headers="headers"
+      :rows="rows"
+      :locale="$i18n.locale"
+      :isLoading="isLoading"
+      @update:pagination="$emit('changePage', $event)"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SingleTable',
+};
+</script>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  title: { type: String, default: '' },
+  pagination: { type: Number, required: true },
+  paginationTotal: { type: Number, required: true },
+  paginationInterval: { type: Number, default: 5 },
+  headers: { type: Array, required: true },
+  rows: { type: Array, required: true },
+  isLoading: { type: Boolean, default: false },
+  hidePagination: { type: Boolean, default: false },
+});
+
+defineEmits(['changePage']);
+
+const paginationCssToken = computed(() =>
+  props.hidePagination ? 'none' : 'flex',
+);
+</script>
+
+<style scoped lang="scss">
+.single-table {
+  display: grid;
+  gap: 10px;
+
+  &__title {
+    color: $unnnic-color-neutral-dark;
+
+    font-family: $unnnic-font-family-secondary;
+    font-size: $unnnic-font-size-body-lg;
+    font-style: normal;
+    font-weight: $unnnic-font-weight-bold;
+    line-height: $unnnic-line-height-large + $unnnic-line-height-medium;
+  }
+
+  &__table {
+    :deep(.table-pagination) {
+      display: v-bind(paginationCssToken);
+    }
+  }
+}
+</style>

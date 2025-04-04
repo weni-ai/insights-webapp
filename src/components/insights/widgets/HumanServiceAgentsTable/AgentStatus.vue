6a486bfbc11676b2ee0d5533fd159f80263101ea
Marcus Vinícius <marcusvinici25@gmail.com>
2025-04-04 14:48:18 -0300

    adding status render logic

--- src/components/insights/widgets/HumanServiceAgentsTable/AgentStatus.vue
+++ src/components/insights/widgets/HumanServiceAgentsTable/AgentStatus.vue
@@ -5,6 +5,12 @@
       size="avatar-nano"
       :class="statusClass"
     />
+    <span
+      v-if="label"
+      class="agent-status-label"
+    >
+      {{ label }}
+    </span>
   </section>
 </template>
 
@@ -16,6 +22,10 @@ const props = defineProps({
     type: String,
     required: true,
     validator: (value) => ['green', 'gray', 'orange'].includes(value),
+  },
+  label: {
+    type: String,
+    default: '',
   },
 });
 
@@ -31,7 +41,19 @@ const statusClass = computed(() => {
 .agent-status-container {
   display: flex;
   gap: $unnnic-spacing-nano;
-  align-items: end;
+  align-items: center;
+}
+
+.agent-status-label {
+  font-family: $unnnic-font-family-secondary;
+  color: $unnnic-color-neutral-dark;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  font-size: $unnnic-font-size-body-gt;
+  font-style: normal;
+  font-weight: $unnnic-font-weight-regular;
+  line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
 }
 
 .agent-status {

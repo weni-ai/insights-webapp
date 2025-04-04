22398380eb81d88050b20c201ce9080433ce7740
Marcus Vinícius <marcusvinici25@gmail.com>
2025-04-04 14:40:03 -0300

    adding new agent status logic

--- src/components/insights/widgets/HumanServiceAgentsTable/AgentStatus.vue
+++ src/components/insights/widgets/HumanServiceAgentsTable/AgentStatus.vue
@@ -3,17 +3,27 @@
     <UnnnicIcon
       icon="indicator"
       size="avatar-nano"
-      :scheme="`feedback-${props.status?.toLowerCase() === 'green' ? 'green' : 'grey'}`"
+      :class="statusClass"
     />
   </section>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
   status: {
     type: String,
     required: true,
+    validator: (value) => ['green', 'gray', 'orange'].includes(value),
   },
+});
+
+const statusClass = computed(() => {
+  return {
+    'agent-status': true,
+    [`agent-status--${props.status}`]: true,
+  };
 });
 </script>
 
@@ -22,5 +32,25 @@ const props = defineProps({
   display: flex;
   gap: $unnnic-spacing-nano;
   align-items: end;
+}
+
+.agent-status {
+  &.agent-status--green {
+    :deep(.primary) {
+      fill: $unnnic-color-aux-green-300;
+    }
+  }
+
+  &.agent-status--gray {
+    :deep(.primary) {
+      fill: $unnnic-color-neutral-cleanest;
+    }
+  }
+
+  &.agent-status--orange {
+    :deep(.primary) {
+      fill: $unnnic-color-aux-orange-500;
+    }
+  }
 }
 </style>

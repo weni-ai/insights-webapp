d9d9093cec991991ffaee69ecc51341a47ea5ae9
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-05-16 16:00:45 -0300

    fix: add missing /

--- rspack.config.js
+++ rspack.config.js
@@ -24,9 +24,9 @@ module.exports = defineConfig({
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: `${process.env.PUBLIC_PATH_URL}`,
-    filename: 'assets/js/[name]-[contenthash].js',
-    chunkFilename: 'assets/js/[name]-[contenthash].js',
-    assetModuleFilename: 'assets/[name]-[hash][ext]',
+    filename: '/assets/js/[name]-[contenthash].js',
+    chunkFilename: '/assets/js/[name]-[contenthash].js',
+    assetModuleFilename: '/assets/[name]-[hash][ext]',
   },
   entry: {
     main: './src/main.js',

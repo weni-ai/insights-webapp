baa130e3653296dac815ac961e3d12f9f0c0b11b
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-05-20 10:27:15 -0300

    chore: update @weni/unnnic-system to version 2.33.1-alpha.0 and refactor date handling in DynamicFilter and FilterDate components

--- src/components/insights/Layout/HeaderFilters/FilterDate.vue
+++ src/components/insights/Layout/HeaderFilters/FilterDate.vue
@@ -6,11 +6,20 @@
     :inputFormat="$t('date_format')"
     size="md"
     position="right"
+    :minDate="minDate"
+    :maxDate="maxDate"
+    @select-date="selectedDates = $event"
     @update:model-value="$emit('update:modelValue', $event)"
   />
 </template>
 
 <script>
+import { useDashboards } from '@/store/modules/dashboards';
+
+import { mapState } from 'pinia';
+
+import moment from 'moment';
+
 export default {
   name: 'FilterDate',
   props: {
@@ -20,6 +29,53 @@ export default {
     },
   },
   emits: ['update:modelValue'],
+  data() {
+    return {
+      selectedDates: { start: '', end: '' },
+    };
+  },
+  computed: {
+    ...mapState(useDashboards, ['currentDashboard']),
+    isMetaTemplateDashboard() {
+      return !!this.currentDashboard?.config?.is_whatsapp_integration;
+    },
+    isHumanServiceDashboard() {
+      return this.currentDashboard?.name === 'human_service_dashboard.title';
+    },
+    minDate() {
+      if (this.isHumanServiceDashboard) return undefined;
+
+      const momentStart = moment(this.selectedDates.start);
+
+      const minDate = momentStart.subtract(89, 'days').format('YYYY-MM-DD');
+
+      const isMinLast90Days =
+        minDate === 'Invalid date' || this.isMetaTemplateDashboard;
+
+      return isMinLast90Days
+        ? moment().subtract(89, 'days').format('YYYY-MM-DD')
+        : minDate;
+    },
+    maxDate() {
+      const today = moment();
+
+      const momentStart = moment(this.selectedDates.start);
+
+      const maxCalculated = momentStart.add(89, 'days');
+
+      const isMaxToday =
+        this.isMetaTemplateDashboard ||
+        this.isHumanServiceDashboard ||
+        momentStart.format('YYYY-MM-DD') === 'Invalid date' ||
+        maxCalculated.isAfter(today);
+
+      if (isMaxToday) {
+        return today.format('YYYY-MM-DD');
+      }
+
+      return maxCalculated.format('YYYY-MM-DD');
+    },
+  },
 };
 </script>
 
@@ -28,5 +84,6 @@ export default {
 .filter-date.dropdown {
   display: grid;
   height: 100%;
+  z-index: 3;
 }
 </style>

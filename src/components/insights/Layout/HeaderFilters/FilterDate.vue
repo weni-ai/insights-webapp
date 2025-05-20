c6d168c9dc4f02543713d1a129e101a3ccfd9a9a
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-05-20 14:09:11 -0300

    refactor(FilterDate): simplify date logic by removing unnecessary checks and adding current month filters

--- src/components/insights/Layout/HeaderFilters/FilterDate.vue
+++ src/components/insights/Layout/HeaderFilters/FilterDate.vue
@@ -36,69 +36,16 @@ export default {
   },
   computed: {
     ...mapState(useDashboards, ['currentDashboard']),
-    isMetaTemplateDashboard() {
-      return !!this.currentDashboard?.config?.is_whatsapp_integration;
-    },
     isHumanServiceDashboard() {
       return this.currentDashboard?.name === 'human_service_dashboard.title';
     },
     minDate() {
       if (this.isHumanServiceDashboard) return undefined;
-      if (this.isMetaTemplateDashboard) {
-        return moment().subtract(89, 'days').format('YYYY-MM-DD');
-      }
-
-      const momentStart = moment(this.selectedDates.start);
-      const momentEnd = moment(this.selectedDates.end);
-
-      const formattedMomentStart = momentStart.format('YYYY-MM-DD');
-      const formattedMomentEnd = momentEnd.format('YYYY-MM-DD');
-
-      const isDiffSelectedDates =
-        formattedMomentStart !== formattedMomentEnd ||
-        (formattedMomentStart === 'Invalid date' &&
-          formattedMomentEnd === 'Invalid date');
-
-      if (isDiffSelectedDates) return undefined;
-
-      const minCalculatedDate = momentStart
-        .subtract(89, 'days')
-        .format('YYYY-MM-DD');
-
-      const isMinLast90Days = minCalculatedDate === 'Invalid date';
-
-      return isMinLast90Days
-        ? moment().subtract(89, 'days').format('YYYY-MM-DD')
-        : minCalculatedDate;
+      return moment().subtract(89, 'days').format('YYYY-MM-DD');
     },
     maxDate() {
       const today = moment();
-
-      const momentStart = moment(this.selectedDates.start);
-      const momentEnd = moment(this.selectedDates.end);
-
-      const formattedMomentStart = momentStart.format('YYYY-MM-DD');
-      const formattedMomentEnd = momentEnd.format('YYYY-MM-DD');
-
-      const isDiffSelectedDates =
-        formattedMomentStart !== formattedMomentEnd ||
-        (formattedMomentStart === 'Invalid date' &&
-          formattedMomentEnd === 'Invalid date');
-
-      const maxCalculated = momentStart.add(89, 'days');
-
-      const isMaxToday =
-        this.isMetaTemplateDashboard ||
-        this.isHumanServiceDashboard ||
-        isDiffSelectedDates ||
-        momentStart.format('YYYY-MM-DD') === 'Invalid date' ||
-        maxCalculated.isAfter(today);
-
-      if (isMaxToday) {
-        return today.format('YYYY-MM-DD');
-      }
-
-      return maxCalculated.format('YYYY-MM-DD');
+      return today.format('YYYY-MM-DD');
     },
   },
 };

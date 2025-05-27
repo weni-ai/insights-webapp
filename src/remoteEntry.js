18492d7024532bd2bb702b1dcfde6fa7765f420b
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-05-27 15:30:48 -0300

    wip: init insights module federation

---
+++ src/remoteEntry.js
@@ -0,0 +1,42 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import Unnnic from './utils/plugins/UnnnicSystem';
+import i18n from './utils/plugins/i18n';
+import { getJwtToken } from './utils/jwt';
+import * as Sentry from '@sentry/vue';
+import env from './utils/env';
+
+import '@weni/unnnic-system/dist/style.css';
+import './styles/global.scss';
+
+export async function mountInsightsApp(containerId) {
+  // await getJwtToken();
+
+  const app = createApp(App);
+  const pinia = createPinia();
+
+  app.use(pinia);
+  app.use(router);
+  app.use(i18n);
+  app.use(Unnnic);
+
+  if (env('SENTRY_DSN')) {
+    Sentry.init({
+      app,
+      dsn: env('SENTRY_DSN'),
+      integrations: [
+        Sentry.browserTracingIntegration({ router }),
+        Sentry.replayIntegration(),
+      ],
+      tracesSampleRate: 1.0,
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
+      environment: env('ENVIRONMENT'),
+    });
+  }
+
+  app.mount(`#${containerId}`);
+  return app;
+}

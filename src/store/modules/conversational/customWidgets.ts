cfd6f328a8e60953b2136ee411684e3b7d08d455
Marcus Vinícius <marcusvinici25@gmail.com>
2025-08-14 19:30:56 -0300

    feat(CustomWidgetsStore): Enhance custom widget management with loading, deletion, and retrieval functionalities

--- src/store/modules/conversational/customWidgets.ts
+++ src/store/modules/conversational/customWidgets.ts
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia';
 import { WidgetType } from '@/models/types/WidgetTypes';
 import WidgetService from '@/services/api/resources/widgets';
+import WidgetConversationalService, {
+  CustomWidgetResponse,
+} from '@/services/api/resources/conversational/widgets';
 import { useWidgets } from '@/store/modules/widgets';
 
 interface customForm {
@@ -9,20 +12,31 @@ interface customForm {
   key: string;
 }
 
+interface customWidget extends WidgetType {
+  data: CustomWidgetResponse;
+}
+
 export const useCustomWidgets = defineStore('customWidgets', {
   state: () => ({
-    customWidgets: [] as WidgetType[],
+    customWidgets: [] as customWidget[],
     customForm: {
       agent_uuid: '',
       agent_name: '',
       key: '',
     } as customForm,
     isLoadingSaveNewCustomWidget: false,
+    isLoadingDeleteCustomWidget: false,
+    loadingByUuid: [] as string[],
   }),
 
   actions: {
     setCustomWidgets(customWidgets: WidgetType[]) {
       this.customWidgets = customWidgets;
+    },
+    updateCustomWidget(uuid: string, data: CustomWidgetResponse) {
+      this.customWidgets = this.customWidgets.map((widget) =>
+        widget.uuid === uuid ? { ...widget, data: data } : widget,
+      );
     },
     setCustomForm(customForm: customForm) {
       this.customForm = customForm;
@@ -33,6 +47,12 @@ export const useCustomWidgets = defineStore('customWidgets', {
     setCustomFormAgent(agent_uuid: string, agent_name: string) {
       this.customForm.agent_uuid = agent_uuid;
       this.customForm.agent_name = agent_name;
+    },
+    getCustomWidgetByUuid(uuid: string): customWidget | null {
+      return this.customWidgets.find((widget) => widget.uuid === uuid) || null;
+    },
+    getIsLoadingByUuid(uuid: string): boolean {
+      return this.loadingByUuid.includes(uuid);
     },
     setCustomFormKey(key: string) {
       this.customForm.key = key;
@@ -64,19 +84,46 @@ export const useCustomWidgets = defineStore('customWidgets', {
 
         await getCurrentDashboardWidgets();
 
-        this.resetCustomForm();
+        this.customForm = {} as customForm;
       } catch (error) {
         console.error('Error saving new custom widget', error);
       } finally {
         this.isLoadingSaveNewCustomWidget = false;
       }
     },
+    async deleteCustomWidget(uuid: string) {
+      this.isLoadingDeleteCustomWidget = true;
+      try {
+        await WidgetService.deleteWidget(uuid);
+
+        const { getCurrentDashboardWidgets } = useWidgets();
+        await getCurrentDashboardWidgets();
+      } catch (error) {
+        console.error('Error deleting widget', error);
+      } finally {
+        this.isLoadingDeleteCustomWidget = false;
+      }
+    },
+    async loadCustomWidgetData(uuid: string) {
+      if (this.loadingByUuid.includes(uuid)) return;
+
+      this.loadingByUuid.push(uuid);
+
+      try {
+        const response = await WidgetConversationalService.getCustomWidgetData({
+          widget_uuid: uuid,
+        });
+        this.updateCustomWidget(uuid, response);
+      } catch (error) {
+        console.error('Error loading custom widget data', error);
+      } finally {
+        this.loadingByUuid = this.loadingByUuid.filter((id) => id !== uuid);
+      }
+    },
   },
   getters: {
     getCustomWidgets: (state) => state.customWidgets,
     getCustomForm: (state) => state.customForm,
-    getByUuid: (state) => (uuid: string) =>
-      state.customWidgets.find((widget) => widget.uuid === uuid),
     isEnabledCreateCustomForm: (state) =>
       state.customForm.agent_uuid?.trim() !== '' &&
       state.customForm.key?.trim() !== '',

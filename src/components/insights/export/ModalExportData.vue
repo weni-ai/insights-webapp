6086be9e01039cbd05ab627e445166953f1ac0fe
Marcus Vinícius <marcusvinici25@gmail.com>
2025-08-19 19:10:53 -0300

    feat(ModalExportData): Add export data modal component with button and dialog functionality

---
+++ src/components/insights/export/ModalExportData.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="modal-export-data">
+    <UnnnicButton
+      type="primary"
+      size="large"
+      text="Export Data"
+      @click="setIsRenderExportData(true)"
+    />
+    <UnnnicModalDialog
+      data-test-id="modal-dialog"
+      :modelValue="isRenderExportData"
+      class="finish-onboarding-modal"
+      :primaryButtonProps="{ text: 'Ok' }"
+      hideSecondaryButton
+      showActionsDivider
+      size="lg"
+      title="Export Data"
+      showCloseIcon
+      @primary-button-click="setIsRenderExportData(false)"
+      @update:model-value="setIsRenderExportData(false)"
+    >
+      <section class="finish-onboarding-modal__content">
+        <h1>Export Data</h1>
+      </section>
+    </UnnnicModalDialog>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useExportData } from '@/store/modules/export/exportData';
+
+const useExportDataStore = useExportData();
+const { setIsRenderExportData } = useExportDataStore;
+const { isRenderExportData } = storeToRefs(useExportDataStore);
+</script>
+
+<style lang="scss" scoped>
+.modal-export-data {
+  display: flex;
+}
+</style>

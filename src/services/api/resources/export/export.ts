af952c3db6a0c616685eae3776ebcfee95709916
Marcus Vinícius <marcusvinici25@gmail.com>
2025-08-25 13:13:30 -0300

    feat(export): Introduce export request and response interfaces, and implement methods for checking export status and creating exports

--- src/services/api/resources/export/export.ts
+++ src/services/api/resources/export/export.ts
@@ -11,6 +11,33 @@ interface ModelFields {
   };
 }
 
+interface ExportEntity {
+  uuids: string[];
+  fields: string[];
+}
+
+interface ExportRequest {
+  project_uuid: string;
+  start_date: string; // YYYY-MM-DD format
+  end_date: string; // YYYY-MM-DD format
+  open_chats: boolean;
+  closed_chats: boolean;
+  type: '.csv' | '.xlsx';
+  sectors: ExportEntity;
+  queues: ExportEntity;
+  rooms: ExportEntity;
+  users: ExportEntity;
+  sector_tags: ExportEntity;
+  contacts: ExportEntity;
+}
+
+interface ExportResponse {
+  status: string;
+  created_at: string;
+  email: string;
+  uuid: string;
+}
+
 export default {
   async getModelFields(): Promise<ModelFields> {
     const { project } = useConfig();
@@ -25,6 +52,33 @@ export default {
 
     return response;
   },
+
+  async checkExportStatus(): Promise<ExportResponse> {
+    const { project } = useConfig();
+
+    const formattedParams = {
+      project_uuid: project.uuid,
+    };
+
+    const response = (await chatsHttp.get(`/chats/report /`, {
+      params: formattedParams,
+    })) as ExportResponse;
+
+    return response;
+  },
+
+  async createExport(
+    exportData: Omit<ExportRequest, 'project_uuid'>,
+  ): Promise<ExportResponse> {
+    const { project } = useConfig();
+
+    const response = (await chatsHttp.post(`/chats/report/`, {
+      ...exportData,
+      project_uuid: project.uuid,
+    })) as ExportResponse;
+
+    return response;
+  },
 };
 
-export type { ModelFields };
+export type { ModelFields, ExportRequest, ExportResponse, ExportEntity };

58ba37201fc7264396cfbd541711d0a9b1451524
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-10 13:12:44 -0300

    feat(ExportFooter): Add ExportFooter component for format selection and terms acceptance in export process

---
+++ src/components/insights/export/ExportFooter.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="export-footer">
+    <section class="export-footer__format">
+      <UnnnicLabel :label="formatLabel" />
+      <UnnnicRadio
+        :data-testid="'radio-format-xlsx'"
+        :modelValue="selectedFormat"
+        value=".xlsx"
+        @update:model-value="handleFormatChange('.xlsx')"
+      >
+        {{ '.XLSX' }}
+      </UnnnicRadio>
+      <UnnnicRadio
+        :data-testid="'radio-format-csv'"
+        :modelValue="selectedFormat"
+        value=".csv"
+        @update:model-value="handleFormatChange('.csv')"
+      >
+        {{ '.CSV' }}
+      </UnnnicRadio>
+    </section>
+
+    <section class="export-footer__terms">
+      <p class="export-footer__terms-warning">
+        <UnnnicIcon
+          icon="alert-circle-1"
+          filled
+          next
+          scheme="feedback-yellow"
+          size="ant"
+        />{{ warningTermsText }}
+      </p>
+      <UnnnicCheckbox
+        :modelValue="acceptTerms"
+        :textRight="acceptTermsText"
+        @update:model-value="handleAcceptTermsChange"
+      />
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+type FormatType = '.csv' | '.xlsx';
+
+interface Props {
+  selectedFormat: FormatType;
+  acceptTerms: boolean;
+  formatLabel: string;
+  warningTermsText: string;
+  acceptTermsText: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  selectedFormat: '.xlsx',
+  acceptTerms: false,
+  formatLabel: '',
+  warningTermsText: '',
+  acceptTermsText: '',
+});
+
+const emit = defineEmits<{
+  'format-change': [format: FormatType];
+  'accept-terms-change': [value: boolean];
+}>();
+
+const handleFormatChange = (format: FormatType): void => {
+  emit('format-change', format);
+};
+
+const handleAcceptTermsChange = (value: boolean): void => {
+  emit('accept-terms-change', value);
+};
+</script>
+
+<style scoped lang="scss">
+.export-footer {
+  display: flex;
+  flex-direction: column;
+  gap: $unnnic-spacing-sm;
+
+  &__format {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-spacing-sm;
+  }
+
+  &__terms {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-spacing-sm;
+  }
+
+  &__terms-warning {
+    display: flex;
+    gap: $unnnic-spacing-xs;
+
+    color: $unnnic-color-neutral-dark;
+    font-family: $unnnic-font-family-secondary;
+    font-size: $unnnic-font-size-body-gt;
+    font-weight: $unnnic-font-weight-regular;
+    line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
+  }
+}
+</style>

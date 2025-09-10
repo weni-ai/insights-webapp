fad19a6b7944aaa906ffb4a1c5e1adf19288e54c
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-10 14:26:13 -0300

    feat(FormExport): Implement Conversational export form with date range selection, format options, and terms acceptance

---
+++ src/components/insights/export/Conversational/FormExport.vue
@@ -0,0 +1,128 @@
+<template>
+  <section class="conversational-export-form">
+    <header>
+      <p class="conversational-export-form__description">
+        {{ $t('export_data.description') }}
+      </p>
+    </header>
+    <section class="conversational-export-form__content">
+      <ExportFilterDate
+        v-model="date_range"
+        :label="$t('export_data.select_period.label')"
+        :placeholder="$t('export_data.select_period.placeholder')"
+        :options="conversationalShortcuts"
+        :minDate="getMinDate()"
+        :maxDate="getMaxDate()"
+        @update:model-value="updateDateRange"
+      />
+
+      <FormCheckbox />
+
+      <ExportFooter
+        :selectedFormat="selectedFormat"
+        :acceptTerms="accept_terms"
+        :formatLabel="$t('export_data.select_format')"
+        :warningTermsText="$t('export_data.warning_terms')"
+        :acceptTermsText="$t('export_data.accept_terms')"
+        @format-change="updateFormat"
+        @accept-terms-change="updateAcceptTerms"
+      />
+    </section>
+  </section>
+</template>
+
+<script setup lang="ts">
+import ExportFilterDate from '../ExportFilterDate.vue';
+import ExportFooter from '../ExportFooter.vue';
+import FormCheckbox from './FormCheckbox.vue';
+import { useConversationalExport } from '@/store/modules/export/conversational/export';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { getLastNDays, getTodayDate } from '@/utils/time';
+
+const conversationalExport = useConversationalExport();
+const { setDateRange, setType, setAcceptTerms } = conversationalExport;
+const { date_range, type, accept_terms } = storeToRefs(conversationalExport);
+
+const { t } = useI18n();
+
+const selectedFormat = computed(() => {
+  return type.value === 'CSV' ? '.csv' : '.xlsx';
+});
+
+const conversationalShortcuts = computed(() => [
+  {
+    name: t('conversational_export.shortcuts.last_3_days'),
+    id: 'last-3-days',
+  },
+  {
+    name: t('conversational_export.shortcuts.last_7_days'),
+    id: 'last-7-days',
+  },
+  {
+    name: t('conversational_export.shortcuts.last_15_days'),
+    id: 'last-15-days',
+  },
+  {
+    name: t('conversational_export.shortcuts.last_30_days'),
+    id: 'last-30-days',
+  },
+  {
+    name: t('conversational_export.shortcuts.current_week'),
+    id: 'current-week',
+  },
+  {
+    name: t('conversational_export.shortcuts.current_month'),
+    id: 'current-month',
+  },
+]);
+
+const getMinDate = (): string => {
+  const minDate = getLastNDays(90).start;
+  return minDate;
+};
+
+const getMaxDate = (): string => {
+  const maxDate = getTodayDate().start;
+  return maxDate;
+};
+
+const updateDateRange = (value: { start: string; end: string }) => {
+  setDateRange(value.start, value.end);
+};
+
+const updateFormat = (format: '.csv' | '.xlsx') => {
+  setType(format === '.csv' ? 'CSV' : 'XLSX');
+};
+
+const updateAcceptTerms = (value: boolean) => {
+  setAcceptTerms(value);
+};
+</script>
+
+<style scoped lang="scss">
+.conversational-export-form {
+  display: flex;
+  flex-direction: column;
+  gap: $unnnic-spacing-sm;
+  background: $unnnic-color-neutral-white;
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-spacing-sm;
+    :deep(.unnnic-label__label) {
+      margin: 0;
+    }
+  }
+
+  &__description {
+    color: $unnnic-color-neutral-cloudy;
+    font-family: $unnnic-font-family-secondary;
+    font-size: $unnnic-font-size-body-gt;
+    font-weight: $unnnic-font-weight-regular;
+    line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
+  }
+}
+</style>

ddb5bad8aa417707acdecd255917c8ba9b01323d
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-10 12:06:33 -0300

    refactor(FormExport): Replace ExportCheckBoxs with FormCheckbox component for improved consistency and clarity

--- src/components/insights/export/HumanResource/FormExport.vue
+++ src/components/insights/export/HumanResource/FormExport.vue
@@ -93,7 +93,7 @@
         </section>
       </section>
 
-      <ExportCheckBoxs />
+      <FormCheckbox />
 
       <section class="export-data-form__format">
         <UnnnicLabel :label="$t('export_data.select_format')" />
@@ -138,7 +138,7 @@
 <script setup lang="ts">
 import FilterDate from '@/components/insights/Layout/HeaderFilters/FilterDate.vue';
 import FilterMultiSelect from '@/components/insights/Layout/HeaderFilters/FilterMultiSelect.vue';
-import ExportCheckBoxs from '../ExportCheckboxs.vue';
+import FormCheckbox from './FormCheckbox.vue';
 import { useHumanResourceExport } from '@/store/modules/export/humanResource/export';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';

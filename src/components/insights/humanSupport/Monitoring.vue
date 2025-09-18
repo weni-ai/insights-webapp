51571e15b2ad8152d081833d0064f65ce0eb3b19
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-18 15:45:40 -0300

    feat(Monitoring): Add Monitoring component for Human Support dashboard

---
+++ src/components/insights/humanSupport/Monitoring.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="monitoring">
+    <h1>Monitoring</h1>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.monitoring {
+  display: flex;
+  flex-direction: column;
+  gap: $unnnic-space-8;
+}
+</style>

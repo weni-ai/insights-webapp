ac0522cc7e13c1376b96c9b4eaa32a92d35ce69d
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-23 12:58:53 -0300

    feat(Monitoring): add Monitoring component with auto-refresh functionality and service status display

---
+++ src/components/insights/humanSupport/Monitoring/Monitoring.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="monitoring">
+    <ServiceStatus />
+    <h1>Monitoring</h1>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+
+import { useDashboards } from '@/store/modules/dashboards';
+import ServiceStatus from './ServiceStatus.vue';
+
+const isLoading = ref(false);
+let autoRefreshInterval: ReturnType<typeof setInterval> | null = null;
+
+const AUTO_REFRESH_INTERVAL = 60 * 1000;
+
+const { updateLastUpdatedRequest } = useDashboards();
+
+const loadData = async () => {
+  if (isLoading.value) {
+    return;
+  }
+
+  try {
+    isLoading.value = true;
+    updateLastUpdatedRequest();
+  } catch (error) {
+    console.error('Erro to get data:', error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+const startAutoRefresh = () => {
+  if (autoRefreshInterval) {
+    clearInterval(autoRefreshInterval);
+  }
+
+  autoRefreshInterval = setInterval(() => {
+    loadData();
+  }, AUTO_REFRESH_INTERVAL);
+};
+
+const stopAutoRefresh = () => {
+  if (autoRefreshInterval) {
+    clearInterval(autoRefreshInterval);
+    autoRefreshInterval = null;
+  }
+};
+
+onMounted(() => {
+  console.log('Monitoring mounted');
+
+  loadData();
+
+  startAutoRefresh();
+});
+
+onUnmounted(() => {
+  console.log('Monitoring unmounted');
+
+  stopAutoRefresh();
+});
+</script>
+
+<style scoped lang="scss">
+.monitoring {
+  display: flex;
+  flex-direction: column;
+  gap: $unnnic-space-8;
+}
+</style>

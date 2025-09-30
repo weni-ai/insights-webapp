0df848cd9831740c1ac450d34269b32b1a89e871
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-30 16:13:31 -0300

    feat(DetailedMonitoring): add DetailedMonitoring component with tabbed interface and filters

---
+++ src/components/insights/humanSupport/Monitoring/DetailedMonitoring.vue
@@ -0,0 +1,119 @@
+<template>
+  <section class="detailed-monitoring">
+    <p class="detailed-monitoring__title">
+      {{ $t('human_support_dashboard.detailed_monitoring.title') }}
+    </p>
+    <Transition name="filters-fade">
+      <section
+        v-if="['attendant', 'pauses'].includes(activeTabName)"
+        class="detailed-monitoring__filters"
+      >
+        <DetailedFilters />
+      </section>
+    </Transition>
+    <section class="detailed-monitoring__tabs">
+      <UnnnicTab
+        data-testid="human-support-tab"
+        :tabs="tabsKeys"
+        :activeTab="activeTabName"
+        @change="changeActiveTabName"
+      >
+        <template
+          v-for="[key, tab] in Object.entries(tabs)"
+          #[`tab-head-${key}`]
+          :key="`tab-head-${key}`"
+        >
+          {{
+            $t(`human_support_dashboard.detailed_monitoring.tabs.${tab.name}`)
+          }}
+        </template>
+        <template
+          v-for="key in Object.keys(tabs)"
+          #[`tab-panel-${key}`]
+          :key="`tab-panel-${key}`"
+        >
+          <component
+            :is="tabs[key].component"
+            :data-testid="`tab-panel-${key}`"
+          />
+        </template>
+      </UnnnicTab>
+    </section>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { UnnnicTab } from '@weni/unnnic-system';
+import InAwaiting from './Tables/InAwaiting.vue';
+import InProgress from './Tables/InProgress.vue';
+import Attendant from './Tables/Attendant.vue';
+import Pauses from './Tables/Pauses.vue';
+import { ref } from 'vue';
+import DetailedFilters from './Filters/DetailedFilters.vue';
+
+const tabs = {
+  in_awaiting: {
+    name: 'in_awaiting',
+    component: InAwaiting,
+  },
+  in_progress: {
+    name: 'in_progress',
+    component: InProgress,
+  },
+  attendant: {
+    name: 'attendant',
+    component: Attendant,
+  },
+  pauses: {
+    name: 'pauses',
+    component: Pauses,
+  },
+};
+
+const tabsKeys = Object.keys(tabs);
+
+const activeTabName = ref('in_progress');
+
+const changeActiveTabName = (tab: string) => {
+  activeTabName.value = tab;
+};
+</script>
+
+<style scoped lang="scss">
+.detailed-monitoring {
+  display: flex;
+  padding: $unnnic-space-6;
+  flex-direction: column;
+  gap: $unnnic-space-6;
+
+  border-radius: 8px;
+  border: 1px solid $unnnic-color-neutral-soft;
+  background: $unnnic-color-background-white;
+
+  &__title {
+    font: $unnnic-font-display-2;
+  }
+
+  &__filters {
+    display: flex;
+    flex-direction: column;
+    flex: 0 0 calc(100% / 4);
+    max-width: calc(100% / 4);
+  }
+}
+
+.filters-fade-enter-active,
+.filters-fade-leave-active {
+  transition: all 0.3s ease;
+}
+
+.filters-fade-enter-from {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.filters-fade-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+</style>

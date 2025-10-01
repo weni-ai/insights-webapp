21df3cb4b94a728eace57f2810d0f04cf5b811cc
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-01 19:49:47 -0300

    feat(InProgress): implement item click redirection functionality to navigate to insights page based on item link

--- src/components/insights/humanSupport/Monitoring/Tables/InProgress.vue
+++ src/components/insights/humanSupport/Monitoring/Tables/InProgress.vue
@@ -15,6 +15,7 @@
     :sort="currentSort"
     @update:sort="handleSort"
     @update:page="handlePageChange"
+    @item-click="redirectItem"
   />
 </template>
 
@@ -93,6 +94,18 @@ const handleSort = (sort: { header: string; order: string }) => {
 const handlePageChange = (newPage: number) => {
   page.value = newPage;
   loadData();
+};
+
+const redirectItem = (item: InProgressDataResult) => {
+  if (!item?.link?.url) return;
+  const path = `${item.link?.url}/insights`;
+  window.parent.postMessage(
+    {
+      event: 'redirect',
+      path,
+    },
+    '*',
+  );
 };
 
 const loadData = async () => {

efdf4a7237134b962e8141b270a8a2211f504e33
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-01 17:57:11 -0300

    feat(AttendantTable): add Attendant component with data table for monitoring agent statuses, including sorting functionality

---
+++ src/components/insights/humanSupport/Monitoring/Tables/Attendant.vue
@@ -0,0 +1,83 @@
+<template>
+  <UnnnicDataTable
+    :locale="$i18n.locale"
+    :isLoading="isLoading"
+    clickable
+    fixedHeaders
+    height="100%"
+    :headers="formattedHeaders"
+    :items="formattedItems"
+    hidePagination
+    data-testid="attendant-table"
+    @update:sort="handleSort"
+  />
+</template>
+
+<script setup lang="ts">
+import { UnnnicDataTable } from '@weni/unnnic-system';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
+
+const isLoading = ref(false);
+
+const formattedHeaders = computed(() => [
+  {
+    title: 'Status',
+    itemKey: 'status',
+    isSortable: true,
+  },
+  {
+    title: 'Agent',
+    itemKey: 'agent',
+    isSortable: true,
+  },
+  {
+    title: 'Opened',
+    itemKey: 'opened',
+    isSortable: true,
+  },
+  {
+    title: 'Closed',
+    itemKey: 'closed',
+    isSortable: true,
+  },
+]);
+
+const formattedItems = computed(() => [
+  {
+    status: 'Online',
+    agent: 'Alice',
+    opened: '10',
+    closed: '5',
+  },
+  {
+    status: 'Offline',
+    agent: 'Bob',
+    opened: '10',
+    closed: '5',
+  },
+  {
+    status: 'Offline',
+    agent: 'Charlie',
+    opened: '10',
+    closed: '5',
+  },
+  {
+    status: 'Offline',
+    agent: 'David',
+    opened: '10',
+    closed: '5',
+  },
+]);
+
+const handleSort = (sort: any) => {
+  console.log('attendant-table sort', sort);
+};
+
+onMounted(() => {
+  console.log('attendant-table mounted');
+});
+
+onUnmounted(() => {
+  console.log('attendant-table unmounted');
+});
+</script>

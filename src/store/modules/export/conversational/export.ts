d250e92468b40883e2507a37985e64a789079ee8
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-22 18:44:01 -0300

    fix: improve error handling in conversational export store
    - Updated error handling to access error messages from the correct data structure in the response.
    - Enhanced user feedback by ensuring specific error messages are displayed based on the updated error response format.

--- src/store/modules/export/conversational/export.ts
+++ src/store/modules/export/conversational/export.ts
@@ -246,13 +246,13 @@ export const useConversationalExport = defineStore('conversationalExport', {
         this.setIsRenderExportData(false);
         this.setIsRenderExportDataFeedback(true);
       } catch (error) {
-        if (error?.status === 400 && error?.concurrent_report) {
+        if (error?.status === 400 && error?.data?.concurrent_report) {
           defaultAlert(
             'error',
             i18n.global.t('export_data.error_pending_export'),
           );
-        } else if (error?.status === 400 && error?.error) {
-          defaultAlert('error', error?.error);
+        } else if (error?.status === 400 && error?.data?.error) {
+          defaultAlert('error', error?.data?.error);
         } else {
           defaultAlert('error', i18n.global.t('export_data.error_default'));
         }

1f9c8ab8532c22a09504b067862387b436642ddb
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-22 14:17:47 -0300

    feat: add DateFilter component for date range selection in human support
    - Introduced a new DateFilter component to allow users to select and apply date ranges for filtering insights.
    - Integrated the component with the humanSupport store to manage the applied date range state.
    - Enhanced user experience by providing a placeholder for the date range input.

---
+++ src/components/insights/humanSupport/Header/DateFilter.vue
@@ -0,0 +1,23 @@
+<template>
+  <FilterDate
+    :modelValue="appliedDateRange"
+    :placeholder="$t('human_support.filters.date_range')"
+    @update:model-value="handleUpdateModelValue"
+  />
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import FilterDate from '@/components/insights/Layout/HeaderFilters/FilterDate.vue';
+import {
+  useHumanSupport,
+  type DateRange,
+} from '@/store/modules/humanSupport/humanSupport';
+
+const humanSupportStore = useHumanSupport();
+const { appliedDateRange } = storeToRefs(humanSupportStore);
+
+const handleUpdateModelValue = (value: DateRange) => {
+  humanSupportStore.appliedDateRange = value;
+};
+</script>

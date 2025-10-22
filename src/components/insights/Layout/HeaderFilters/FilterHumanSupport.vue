0e6bf903bde5b8eab4b07db09d7632be1bb7e067
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-22 14:55:22 -0300

    feat: conditionally render DateFilter based on active tab state
    - Updated FilterHumanSupport component to conditionally display the DateFilter component when the active tab is set to 'analysis'.
    - Integrated computed property to determine the active tab state from the humanSupport store, enhancing component reactivity.

--- src/components/insights/Layout/HeaderFilters/FilterHumanSupport.vue
+++ src/components/insights/Layout/HeaderFilters/FilterHumanSupport.vue
@@ -1,9 +1,17 @@
 <template>
-  <DateFilter />
+  <DateFilter v-if="isAnalysis" />
   <FiltersDropdown />
 </template>
 
 <script setup lang="ts">
 import FiltersDropdown from '@/components/insights/humanSupport/Header/FiltersDropdown.vue';
 import DateFilter from '@/components/insights/humanSupport/Header/DateFilter.vue';
+import { useHumanSupport } from '@/store/modules/humanSupport/humanSupport';
+import { computed } from 'vue';
+
+const humanSupportStore = useHumanSupport();
+
+const isAnalysis = computed(() => {
+  return humanSupportStore.activeTab === 'analysis';
+});
 </script>

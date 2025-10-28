5ba72700190988076522288f6070a38d4d7aca39
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-28 00:05:15 -0300

    feat(humanSupport): add Finished table component for detailed analysis
    - Introduced a new Finished.vue component to display detailed analysis data in a table format.
    - Implemented sorting, pagination, and data loading functionalities.
    - Utilized the UnnnicDataTable component for enhanced user interaction and data presentation.

---
+++ src/components/insights/humanSupport/Analysis/Tables/Finished.vue
@@ -0,0 +1,152 @@
+<template>
+  <UnnnicDataTable
+    :locale="$i18n.locale"
+    :isLoading="isLoading"
+    clickable
+    fixedHeaders
+    height="100%"
+    :headers="formattedHeaders"
+    :items="formattedItems"
+    :page="page"
+    :pageTotal="pageTotal"
+    :pageInterval="pageInterval"
+    data-testid="in-progress-table"
+    size="sm"
+    :sort="currentSort"
+    @update:sort="handleSort"
+    @update:page="handlePageChange"
+    @item-click="redirectItem"
+  />
+</template>
+
+<script setup lang="ts">
+import { UnnnicDataTable } from '@weni/unnnic-system';
+import { computed, onMounted, ref, watch } from 'vue';
+import { FinishedDataResult } from '@/services/api/resources/humanSupport/analysis/detailedAnalysis/finished';
+import service from '@/services/api/resources/humanSupport/analysis/detailedAnalysis/finished';
+import { useI18n } from 'vue-i18n';
+import { useHumanSupport } from '@/store/modules/humanSupport/humanSupport';
+import { formatSecondsToTime } from '@/utils/time';
+
+type FormattedInProgressData = Omit<
+  FinishedDataResult,
+  'duration' | 'awaiting_time' | 'first_response_time' | 'response_time'
+> & {
+  duration: string;
+  awaiting_time: string;
+  first_response_time: string;
+  response_time: string;
+};
+
+const { t } = useI18n();
+const humanSupport = useHumanSupport();
+const isLoading = ref(false);
+
+const page = ref(1);
+const pageInterval = ref(15);
+const pageTotal = ref(0);
+
+const currentSort = ref<{ header: string; itemKey: string; order: string }>({
+  header: 'first_response_time',
+  order: 'desc',
+  itemKey: 'first_response_time',
+});
+
+const formattedItems = ref<FormattedInProgressData[]>([]);
+
+const formattedHeaders = computed(() => {
+  const baseTranslationKey = 'human_support_dashboard.columns.common';
+
+  const createHeader = (itemKey: string, translationKey?: string) => ({
+    title: t(`${baseTranslationKey}.${translationKey || itemKey}`),
+    itemKey,
+    isSortable: true,
+  });
+
+  return [
+    createHeader('agent'),
+    createHeader('sector'),
+    createHeader('queue'),
+    createHeader('awaiting_time'),
+    createHeader('first_response_time'),
+    createHeader('duration'),
+    createHeader('contact'),
+    createHeader('ticket_id'),
+  ];
+});
+
+const handleSort = (sort: {
+  header: string;
+  itemKey: string;
+  order: string;
+}) => {
+  currentSort.value = sort;
+};
+
+const handlePageChange = (newPage: number) => {
+  page.value = newPage;
+  loadData();
+};
+
+const redirectItem = (item: FinishedDataResult) => {
+  if (!item?.link?.url) return;
+  const path = `${item.link?.url}/insights`;
+  window.parent.postMessage(
+    {
+      event: 'redirect',
+      path,
+    },
+    '*',
+  );
+};
+
+const loadData = async () => {
+  try {
+    isLoading.value = true;
+
+    const offset = (page.value - 1) * pageInterval.value;
+    const ordering =
+      currentSort.value.order === 'desc'
+        ? `-${currentSort.value.itemKey}`
+        : currentSort.value.itemKey;
+
+    const data = await service.getDetailedAnalysisFinishedData({
+      ordering,
+      limit: pageInterval.value,
+      offset,
+    });
+
+    formattedItems.value = data.results.map((result) => ({
+      ...result,
+      duration: formatSecondsToTime(result?.duration),
+      awaiting_time: formatSecondsToTime(result?.awaiting_time),
+      first_response_time: formatSecondsToTime(result?.first_response_time),
+      response_time: formatSecondsToTime(result?.response_time),
+    }));
+
+    pageTotal.value = data.count;
+  } catch (error) {
+    console.error('Error loading in-progress data:', error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  loadData();
+});
+
+watch(currentSort, () => {
+  page.value = 1;
+  loadData();
+});
+
+watch(
+  () => humanSupport.appliedFilters,
+  () => {
+    page.value = 1;
+    loadData();
+  },
+  { flush: 'post' },
+);
+</script>

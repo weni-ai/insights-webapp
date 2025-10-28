364d46f0e5fb3d57d9b88a9566d95590f4cfaf92
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-28 00:02:42 -0300

    feat(humanSupport): add getDetailedAnalysisFinishedData API method
    - Introduced a new method to fetch detailed analysis data for finished tickets, including query parameters for filtering results.
    - Defined interfaces for the response structure and query parameters to ensure type safety and clarity in data handling.

---
+++ src/services/api/resources/humanSupport/analysis/detailedAnalysis/finished.ts
@@ -0,0 +1,80 @@
+import http from '@/services/api/http';
+import { useConfig } from '@/store/modules/config';
+import { useHumanSupport } from '@/store/modules/humanSupport/humanSupport';
+import { useDashboards } from '@/store/modules/dashboards';
+import { createRequestQuery } from '@/utils/request';
+
+interface FinishedDataResult {
+  agent: string;
+  sector: string;
+  queue: string;
+  awaiting_time: number;
+  first_response_time: number;
+  response_time: number;
+  duration: number;
+  contact: string;
+  ticket_id: string;
+  link: {
+    url: string;
+    type: string;
+  };
+}
+
+interface FinishedData {
+  results: FinishedDataResult[];
+  count: number;
+  next: string;
+  previous: string;
+}
+
+interface QueryParams {
+  sectors?: string[];
+  queues?: string[];
+  tags?: string[];
+  start_date?: string;
+  end_date?: string;
+  contact?: string;
+  ticket_id?: string;
+  agent?: string;
+  ordering?: string;
+  limit?: number;
+  offset?: number;
+}
+
+export default {
+  async getDetailedAnalysisFinishedData(
+    queryParams: QueryParams = {},
+  ): Promise<FinishedData> {
+    const { project } = useConfig();
+    const { appliedFilters, appliedDateRange } = useHumanSupport();
+    const { currentDashboard } = useDashboards();
+
+    const formattedAppliedFilters = {
+      sectors: appliedFilters.sectors.map((sector) => sector.value),
+      queues: appliedFilters.queues.map((queue) => queue.value),
+      tags: appliedFilters.tags.map((tag) => tag.value),
+      ordering: queryParams.ordering ? queryParams.ordering : 'response_time',
+    };
+
+    const params = createRequestQuery(queryParams);
+
+    const formattedParams = {
+      project_uuid: project.uuid,
+      start_date: appliedDateRange.start,
+      end_date: appliedDateRange.end,
+      ...formattedAppliedFilters,
+      ...params,
+    };
+
+    const response = (await http.get(
+      `/dashboards/${currentDashboard.uuid}/finished/`,
+      {
+        params: formattedParams,
+      },
+    )) as FinishedData;
+
+    return response;
+  },
+};
+
+export type { FinishedData, FinishedDataResult, QueryParams };

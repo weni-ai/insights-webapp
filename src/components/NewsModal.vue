95af493cacbbc60c675e477537dbf76fdb8eeda1
Marcus Vinícius <marcusvinici25@gmail.com>
2025-11-04 18:03:24 -0300

    feat(components): add NewsModal component for displaying news items with pagination
    - Introduced a new NewsModal component that presents news items with descriptions and images.
    - Implemented pagination functionality to navigate through news items efficiently.

---
+++ src/components/NewsModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <UnnnicModalDialog
+    :modelValue="modelValue"
+    :title="title"
+    showCloseIcon
+  >
+    <section>
+      <template
+        v-for="(item, index) in news"
+        :key="index"
+      >
+        <p v-html="item.description" />
+        <img
+          :src="item.image"
+          alt="news image"
+        />
+      </template>
+      <UnnnicPagination
+        :modelValue="page"
+        :max="news.length"
+        :show="pageSize"
+        @update:model-value="updatePage"
+      />
+    </section>
+  </UnnnicModalDialog>
+</template>
+
+<script setup lang="ts">
+import { UnnnicPagination, UnnnicModalDialog } from '@weni/unnnic-system';
+import { ref } from 'vue';
+defineProps<{
+  modelValue: boolean;
+  title: string;
+  news: {
+    description: string;
+    image: string;
+  }[];
+}>();
+
+const page = ref(1);
+const pageSize = ref(5);
+
+const updatePage = (value: number) => {
+  page.value = value;
+};
+</script>

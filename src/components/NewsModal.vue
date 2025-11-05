bc1a30c5a0e2f7ce9e8abf79c86811f2fb387880
Marcus Vinícius <marcusvinici25@gmail.com>
2025-11-04 21:02:08 -0300

    feat(components): enhance NewsModal with improved structure and pagination
    - Refactored NewsModal to utilize a computed property for current news item selection.
    - Updated the modal to display a second description and improved image handling.
    - Enhanced styling for better layout and user experience, including new CSS classes for content, description, image, and pagination.

--- src/components/NewsModal.vue
+++ src/components/NewsModal.vue
@@ -3,20 +3,25 @@
     :modelValue="modelValue"
     :title="title"
     showCloseIcon
-    @close="emit('close')"
+    size="large"
+    @update:model-value="emit('close')"
   >
-    <section>
-      <template
-        v-for="(item, index) in news"
-        :key="index"
-      >
-        <p v-html="item.description" />
-        <img
-          :src="item.image"
-          alt="news image"
+    <section class="news-modal__content">
+      <section class="news-modal__description">
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <p v-html="currentNewsItem.description" />
+        <p
+          v-if="currentNewsItem.secondDescription"
+          v-html="currentNewsItem.secondDescription"
         />
-      </template>
+      </section>
+      <img
+        class="news-modal__image"
+        :src="currentNewsItem.image"
+        :alt="currentNewsItem.description"
+      />
       <UnnnicPagination
+        class="news-modal__pagination"
         :modelValue="page"
         :max="news.length"
         :show="pageSize"
@@ -28,7 +33,7 @@
 
 <script setup lang="ts">
 import { UnnnicPagination, UnnnicModalDialog } from '@weni/unnnic-system';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const emit = defineEmits<{
   (e: 'close'): void;
@@ -37,16 +42,49 @@ const emit = defineEmits<{
 const props = defineProps<{
   modelValue: boolean;
   title: string;
-  news: {
+  news: Array<{
     description: string;
+    secondDescription: string;
     image: string;
-  }[];
+  }>;
 }>();
 
 const page = ref(1);
-const pageSize = ref(5);
+const pageSize = ref(props.news.length);
+
+const currentNewsItem = computed(() => {
+  return props.news[page.value - 1];
+});
 
 const updatePage = (value: number) => {
   page.value = value;
 };
 </script>
+
+<style scoped lang="scss">
+.news-modal {
+  &__content {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-space-4;
+    justify-content: space-between;
+    max-width: 750px;
+  }
+
+  &__description {
+    display: flex;
+    flex-direction: column;
+    gap: $unnnic-space-4;
+  }
+
+  &__image {
+    max-height: 450px;
+    align-self: stretch;
+    aspect-ratio: 16/9;
+  }
+
+  &__pagination {
+    justify-content: center;
+  }
+}
+</style>

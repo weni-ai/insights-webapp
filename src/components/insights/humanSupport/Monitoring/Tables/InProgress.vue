780cd899abc982fff4b7fdd563438d5edf578f94
Marcus Vinícius <marcusvinici25@gmail.com>
2026-01-15 18:08:07 -0300

    fix: enhance reactivity for contact input filter in monitoring tables
    - Updated watch properties in InAwaiting and InProgress components to include contactInput for improved filtering.
    - Set deep watch option to ensure changes in contactInput trigger data reload.

--- src/components/insights/humanSupport/Monitoring/Tables/InProgress.vue
+++ src/components/insights/humanSupport/Monitoring/Tables/InProgress.vue
@@ -133,11 +133,15 @@ onMounted(() => {
 });
 
 watch(
-  [currentSort, () => humanSupport.appliedFilters],
+  [
+    currentSort,
+    () => humanSupport.appliedFilters,
+    () => humanSupport.appliedDetailFilters.contactInput,
+  ],
   () => {
     resetAndLoadData(currentSort.value);
   },
-  { flush: 'post' },
+  { flush: 'post', deep: true },
 );
 
 watch(

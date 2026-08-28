b2e44d85138af30653238e88b177f4ca8fe6c576
Marcus Vinícius <marcusvinici25@gmail.com>
2026-08-28 11:08:10 -0300

    refactor: remove unused JWT token parsing and retrieval logic
    ### What Changed
    - Removed the `getJwtToken` function and associated logic from `jwt.js`, streamlining the JWT utility functions.
    
    This cleanup eliminates unnecessary complexity and focuses on the essential functionality of JWT parsing.

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,3 @@
-import { moduleStorage } from '@/utils/storage';
-
 export function parseJwt(token) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
@@ -14,22 +12,4 @@ export function parseJwt(token) {
   );
 
   return JSON.parse(jsonPayload);
-}
-
-export function getJwtToken() {
-  return new Promise((resolve) => {
-    const isInIframe = window.self !== window.top;
-
-    if (!isInIframe) resolve();
-
-    const eventHandler = (event) => {
-      if (event.data.event === 'updateToken') {
-        moduleStorage.setItem('token', event.data.token);
-        window.removeEventListener('message', eventHandler);
-        return resolve();
-      }
-    };
-    window.addEventListener('message', eventHandler);
-    window.parent.postMessage({ event: 'getToken' }, '*');
-  });
 }
